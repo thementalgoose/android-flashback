8cb8a6e80daeced1234ab8fec175949d408ed3ed
Jordan Fisher <thementalgoose@gmail.com>
2020-04-27 17:34:33 +0100

    Updated current year default and strings value

--- app/src/main/java/tmg/flashback/Constants.kt
+++ app/src/main/java/tmg/flashback/Constants.kt
@@ -6,7 +6,7 @@ import org.threeten.bp.format.DateTimeFormatter
 const val minimumSupportedYear = 2000
 
 // TODO: Figure out a good way to do this
-val currentYear = Year.now().value
+const val currentYear = 2019 // Year.now().value
 val supportedYears: List<Int> = List(currentYear - minimumSupportedYear + 1) { minimumSupportedYear + it }
 
 val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy")
