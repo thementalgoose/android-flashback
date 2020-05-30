c4a1cda939bc681e774591b8495b3bf37da2e9a9
Jordan Fisher <thementalgoose@gmail.com>
2020-05-30 19:43:44 +0100

    Added separate node for history

--- repo-firebase/src/main/java/tmg/flashback/repo_firebase/repos/HistoryFirestore.kt
+++ repo-firebase/src/main/java/tmg/flashback/repo_firebase/repos/HistoryFirestore.kt
@@ -7,10 +7,12 @@ import kotlinx.coroutines.flow.map
 import tmg.flashback.repo.db.CrashReporter
 import tmg.flashback.repo.db.HistoryDB
 import tmg.flashback.repo.models.History
+import tmg.flashback.repo.models.WinnerSeason
 import tmg.flashback.repo_firebase.converters.convert
 import tmg.flashback.repo_firebase.firebase.FirebaseRepo
 import tmg.flashback.repo_firebase.firebase.getDocument
 import tmg.flashback.repo_firebase.models.FHistorySeason
+import tmg.flashback.repo_firebase.models.FWinner
 import tmg.flashback.repo_firebase.version
 
 class HistoryFirestore(
@@ -26,9 +28,24 @@ class HistoryFirestore(
             .map { list -> list.firstOrNull { it.season == season } }
     }
 
+    override fun allWinners(): Flow<List<WinnerSeason>> {
+        return getWinner()
+    }
+
+    override fun winnersFor(season: Int): Flow<WinnerSeason?> {
+        return getWinner()
+            .map { list -> list.firstOrNull { it.season == season } }
+    }
+
     private fun getHistory(): Flow<List<History>> {
         return document("version/$version/history/season")
             .getDoc<FHistorySeason>()
+            .convertModel { it?.convert() ?: emptyList() }
+    }
+
+    private fun getWinner(): Flow<List<WinnerSeason>> {
+        return document("version/$version/history/winner")
+            .getDoc<FWinner>()
             .convertModel { it?.convert() ?: emptyList() }
     }
 }
--- repo/src/main/java/tmg/flashback/repo/db/HistoryDB.kt
+++ repo/src/main/java/tmg/flashback/repo/db/HistoryDB.kt
@@ -2,8 +2,11 @@ package tmg.flashback.repo.db
 
 import kotlinx.coroutines.flow.Flow
 import tmg.flashback.repo.models.History
+import tmg.flashback.repo.models.WinnerSeason
 
 interface HistoryDB {
     fun allHistory(): Flow<List<History>>
     fun historyFor(season: Int): Flow<History?>
+    fun allWinners(): Flow<List<WinnerSeason>>
+    fun winnersFor(season: Int): Flow<WinnerSeason?>
 }
