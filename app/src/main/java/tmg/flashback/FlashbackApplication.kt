4eb876a8e861f5189d55b1c276efc0d2d50cbb11
Jordan Fisher <thementalgoose@gmail.com>
2020-06-20 15:19:47 +0100

    Bumped version to 4.3.0, cleaned up about.

--- app/src/main/java/tmg/flashback/FlashbackApplication.kt
+++ app/src/main/java/tmg/flashback/FlashbackApplication.kt
@@ -14,6 +14,7 @@ import tmg.flashback.repo.db.CrashReporter
 import tmg.flashback.repo.db.PrefsDB
 
 val releaseNotes: Map<Int, Int> = mapOf(
+    10 to R.string.release_10,
     8 to R.string.release_8,
     7 to R.string.release_7,
     6 to R.string.release_6,
