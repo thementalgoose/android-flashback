08d1a27e00a9673c0868e6f7f964b6d0ebf6be91
Jordan Fisher <thementalgoose@gmail.com>
2020-09-10 21:55:02 +0100

    Adds DriverFirestore structure

--- app/src/main/java/tmg/flashback/di/FirebaseModule.kt
+++ app/src/main/java/tmg/flashback/di/FirebaseModule.kt
@@ -2,17 +2,11 @@ package tmg.flashback.di
 
 import org.koin.dsl.module
 import tmg.flashback.firebase.CrashReporterFirebase
-import tmg.flashback.firebase.repos.CircuitFirestore
-import tmg.flashback.firebase.repos.DataFirestore
-import tmg.flashback.firebase.repos.HistoryFirestore
-import tmg.flashback.firebase.repos.SeasonOverviewFirestore
+import tmg.flashback.firebase.repos.*
 import tmg.flashback.prefs.SharedPrefsDB
 import tmg.flashback.repo.db.CrashReporter
 import tmg.flashback.repo.db.PrefsDB
-import tmg.flashback.repo.db.stats.CircuitDB
-import tmg.flashback.repo.db.stats.DataDB
-import tmg.flashback.repo.db.stats.HistoryDB
-import tmg.flashback.repo.db.stats.SeasonOverviewDB
+import tmg.flashback.repo.db.stats.*
 
 val firebaseModule = module {
 
@@ -22,4 +16,5 @@ val firebaseModule = module {
     single<CrashReporter> { CrashReporterFirebase(get(), get()) }
     single<CircuitDB> { CircuitFirestore(get()) }
     single<DataDB> { DataFirestore(get()) }
+    single<DriverDB> { DriverFirestore(get()) }
 }
---
+++ firebase/src/main/java/tmg/flashback/firebase/repos/DriverFirestore.kt
@@ -0,0 +1,24 @@
+package tmg.flashback.firebase.repos
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import tmg.flashback.firebase.FirebaseRepo
+import tmg.flashback.firebase.converters.convert
+import tmg.flashback.firebase.models.FCircuit
+import tmg.flashback.firebase.models.FDriverOverview
+import tmg.flashback.repo.db.CrashReporter
+import tmg.flashback.repo.db.stats.DriverDB
+import tmg.flashback.repo.models.stats.DriverOverview
+
+class DriverFirestore(
+        crashReporter: CrashReporter
+) : FirebaseRepo(crashReporter), DriverDB {
+
+    override fun getDriverOverview(driverId: String): Flow<DriverOverview?> {
+        return document("drivers/$driverId")
+                .getDoc<FDriverOverview>()
+                .map {
+                    it?.convert()
+                }
+    }
+}
