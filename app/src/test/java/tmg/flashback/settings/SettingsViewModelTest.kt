aced6650722c66f8ea44956dbab5446f48cbf5d5
Jordan Fisher <thementalgoose@gmail.com>
2020-09-11 19:52:41 +0100

    Added mockito dependency for unit testing

---
+++ app/src/test/java/tmg/flashback/settings/SettingsViewModelTest.kt
@@ -0,0 +1,22 @@
+package tmg.flashback.settings
+
+import android.content.Context
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+
+class SettingsViewModelTest {
+
+    lateinit var sut: SettingsViewModel
+
+//    val mockContext: Context = Mickito()
+
+//    @BeforeEach
+//    internal fun setUp() {
+//        sut = SettingsViewModel()
+//    }
+//
+//    @Test
+//    fun
+}
