5fe2751f04ddc6b55200ff8b92c04e437e21e107
Jordan Fisher <thementalgoose@gmail.com>
2020-09-11 19:48:32 +0100

    Added driver overview item view model

--- app/src/main/java/tmg/flashback/driver/overview/DriverOverviewViewModel.kt
+++ app/src/main/java/tmg/flashback/driver/overview/DriverOverviewViewModel.kt
@@ -1,13 +1,16 @@
 package tmg.flashback.driver.overview
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import tmg.flashback.base.BaseViewModel
+import tmg.flashback.repo.models.stats.DriverOverview
 
 //region Inputs
 
 interface DriverOverviewViewModelInputs {
-
+    fun setup(driverId: String)
 }
 
 //endregion
@@ -15,7 +18,7 @@ interface DriverOverviewViewModelInputs {
 //region Outputs
 
 interface DriverOverviewViewModelOutputs {
-
+    val list: LiveData<List<DriverOverviewItem>>
 }
 
 //endregion
@@ -28,11 +31,18 @@ class DriverOverviewViewModel: BaseViewModel(), DriverOverviewViewModelInputs, D
     var inputs: DriverOverviewViewModelInputs = this
     var outputs: DriverOverviewViewModelOutputs = this
 
+    override val list: LiveData<List<DriverOverviewItem>>
+        get() = MutableLiveData()
+
     init {
 
     }
 
     //region Inputs
+
+    override fun setup(driverId: String) {
+
+    }
 
     //endregion
 
