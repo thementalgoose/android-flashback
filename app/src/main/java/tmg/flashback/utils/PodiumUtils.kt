ae78bb93abb3cda79894da494e2b45e0d0407b1c
Jordan Fisher <thementalgoose@gmail.com>
2020-09-12 19:04:26 +0100

    Updated podium utils tests

--- app/src/main/java/tmg/flashback/utils/PodiumUtils.kt
+++ app/src/main/java/tmg/flashback/utils/PodiumUtils.kt
@@ -6,21 +6,34 @@ import tmg.flashback.R
 import tmg.utilities.extensions.fromHtml
 
 fun Int.position(podiumOnly: Boolean = false): Spanned {
-    return when (this) {
-        1 -> "1st".fromHtml()
-        2 -> "2nd".fromHtml()
-        3 -> "3rd".fromHtml()
-        else -> if (podiumOnly) "".fromHtml() else "${this}th".fromHtml()
+    if (podiumOnly) {
+        return when (this) {
+            1 -> "1st".fromHtml()
+            2 -> "2nd".fromHtml()
+            3 -> "3rd".fromHtml()
+            else -> "".fromHtml()
+        }
+    }
+    else {
+        return when (this) {
+            11 -> "11th".fromHtml()
+            12 -> "12th".fromHtml()
+            13 -> "13th".fromHtml()
+            else -> {
+                when (this.toString().lastOrNull()) {
+                    '1' -> "${this}st".fromHtml()
+                    '2' -> "${this}nd".fromHtml()
+                    '3' -> "${this}rd".fromHtml()
+                    null -> "".fromHtml()
+                    else -> "${this}th".fromHtml()
+                }
+            }
+        }
     }
 }
 
 fun Int.podium(): Spanned {
-    return when (this) {
-        1 -> "1st".fromHtml()
-        2 -> "2nd".fromHtml()
-        3 -> "3rd".fromHtml()
-        else -> "${this}th".fromHtml()
-    }
+    return this.position(false)
 }
 
 fun Int.positionStarted(context: Context): String {
