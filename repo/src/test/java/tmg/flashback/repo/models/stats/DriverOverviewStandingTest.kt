71d18e7c049d366815f84a549decdb0b75ad056f
Jordan Fisher <thementalgoose@gmail.com>
2020-09-17 23:24:22 +0100

    Added todo test blocks in driver overview models to remind me to do them at some point

---
+++ repo/src/test/java/tmg/flashback/repo/models/stats/DriverOverviewStandingTest.kt
@@ -0,0 +1,14 @@
+package tmg.flashback.repo.models.stats
+
+import org.junit.jupiter.api.Test
+
+class DriverOverviewStandingTest {
+
+    // TODO: Implement all the tests for these classes
+
+    @Test
+    fun `DriverOverviewStanding championship calculated variable is accurate`() {
+
+        TODO("Not implemented")
+    }
+}
---
+++ repo/src/test/java/tmg/flashback/repo/models/stats/DriverOverviewTest.kt
@@ -0,0 +1,14 @@
+package tmg.flashback.repo.models.stats
+
+import org.junit.jupiter.api.Test
+
+class DriverOverviewTest {
+
+    // TODO: Implement all the tests for these classes
+
+    @Test
+    fun `DriverOverview championship calculated variable is accurate`() {
+
+        TODO("Not implemented")
+    }
+}
