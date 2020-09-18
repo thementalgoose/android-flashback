8a5ac4ddf5bede5044a4dac7a76462241cd8c61e
Jordan Fisher <thementalgoose@gmail.com>
2020-09-18 23:11:56 +0100

    Removed un-necessary method from podium utils

--- app/src/main/java/tmg/flashback/utils/PodiumUtils.kt
+++ app/src/main/java/tmg/flashback/utils/PodiumUtils.kt
@@ -10,10 +10,6 @@ fun Int.position(): String {
     return this.ordinalAbbreviation
 }
 
-fun Int.podium(): String {
-    return this.position()
-}
-
 fun Int.positionStarted(context: Context): String {
     return context.getString(R.string.round_podium_started, this.position())
 }
