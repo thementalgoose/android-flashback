d9f688ead6280ed5a0ff805b0d7cc236666a5284
Jordan Fisher <thementalgoose@gmail.com>
2020-09-20 16:16:13 +0100

    Added touching file for verify json file contents

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -3,6 +3,7 @@
 GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/app/src/live/google-services.json
 
 echo "Updating Google Json"
+touch $GOOGLE_JSON_FILE
 echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
 echo "Verifying JSON file contents"
 cat $GOOGLE_JSON_FILE
