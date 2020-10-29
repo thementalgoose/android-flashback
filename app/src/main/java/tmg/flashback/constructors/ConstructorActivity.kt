791677f6f9b70e11d19144de2d71d3e1bdb38ca1
Jordan Fisher <thementalgoose@gmail.com>
2020-10-29 23:49:55 +0000

    Adds framework for constructor activity

---
+++ app/src/main/java/tmg/flashback/constructors/ConstructorActivity.kt
@@ -0,0 +1,38 @@
+package tmg.flashback.constructors
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.android.synthetic.main.activity_constructor.*
+import org.koin.android.viewmodel.ext.android.viewModel
+import tmg.flashback.R
+import tmg.flashback.base.BaseActivity
+
+class ConstructorActivity: BaseActivity() {
+
+    private val viewModel: ConstructorViewModel by viewModel()
+
+    override fun layoutId(): Int = R.layout.activity_constructor
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun setInsets(insets: WindowInsetsCompat) {
+        titlebar.setPadding(0, insets.systemWindowInsetTop, 0, 0)
+        list.setPadding(0, 0, 0, insets.systemWindowInsetBottom)
+    }
+
+    companion object {
+        private const val keyConstructorId: String = "constructorId"
+        private const val keyConstructorName: String = "constructorName"
+
+        fun intent(context: Context, constructorId: String, constructorName: String): Intent {
+            val intent = Intent(context, ConstructorActivity::class.java)
+            intent.putExtra(keyConstructorId, constructorId)
+            intent.putExtra(keyConstructorName, constructorName)
+            return intent
+        }
+    }
+}
---
+++ app/src/main/java/tmg/flashback/constructors/ConstructorViewModel.kt
@@ -0,0 +1,41 @@
+package tmg.flashback.constructors
+
+import tmg.flashback.base.BaseViewModel
+import tmg.flashback.di.async.ScopeProvider
+
+//region Inputs
+
+interface ConstructorViewModelInputs {
+
+}
+
+//endregion
+
+//region Outputs
+
+interface ConstructorViewModelOutputs {
+
+}
+
+//endregion
+
+
+class ConstructorViewModel(
+    scopeProvider: ScopeProvider
+): BaseViewModel(scopeProvider), ConstructorViewModelInputs, ConstructorViewModelOutputs {
+
+    var inputs: ConstructorViewModelInputs = this
+    var outputs: ConstructorViewModelOutputs = this
+
+    init {
+
+    }
+
+    //region Inputs
+
+    //endregion
+
+    //region Outputs
+
+    //endregion
+}
