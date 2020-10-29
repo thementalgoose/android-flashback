6907124dc212b64cc102069324c630cfba9f47d1
Jordan Fisher <thementalgoose@gmail.com>
2020-10-29 23:39:52 +0000

    Adds not found screen to constructor standings for seasons of 1957 and below when constructor standings were not scored

---
+++ app/src/main/java/tmg/flashback/shared/viewholders/ConstructorsChampionshipNotAwardedViewHolder.kt
@@ -0,0 +1,6 @@
+package tmg.flashback.shared.viewholders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ConstructorsChampionshipNotAwardedViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
