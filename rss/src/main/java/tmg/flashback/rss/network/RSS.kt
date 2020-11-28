bd8c31dfa07b49426e3e157161f25a0491673373
Jordan Fisher <thementalgoose@gmail.com>
2020-11-28 16:03:12 +0000

    Handling SSL Handshake exception gracefully

--- rss/src/main/java/tmg/flashback/rss/network/RSS.kt
+++ rss/src/main/java/tmg/flashback/rss/network/RSS.kt
@@ -16,6 +16,7 @@ import tmg.flashback.rss.prefs.RSSPrefsDB
 import java.lang.NullPointerException
 import java.lang.RuntimeException
 import java.net.UnknownHostException
+import javax.net.ssl.SSLHandshakeException
 import javax.xml.stream.XMLStreamException
 
 class RSS(
@@ -33,6 +34,8 @@ class RSS(
                     val response = xmlRetrofit.getRssXML(x).convert(x, prefsDB.rssShowDescription)
                     responses.add(Response(response))
                 } catch (e: XMLStreamException) {
+                    responses.add(Response(null, -1))
+                } catch (e: SSLHandshakeException) {
                     responses.add(Response(null, -1))
                 } catch (e: RuntimeException) {
                     responses.add(Response(null, -1))
