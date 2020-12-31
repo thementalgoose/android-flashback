e3a24c439f6946151ff810b8a8cdb4f63f36e52d
Jordan Fisher <thementalgoose@gmail.com>
2020-12-31 21:38:14 +0000

    Cleaned up old season list bottom sheet code, added more remote config variables. Added unknown venue image

--- app-firebase/src/main/java/tmg/flashback/firebase/models/FUpNext.kt
+++ app-firebase/src/main/java/tmg/flashback/firebase/models/FUpNext.kt
@@ -5,10 +5,12 @@ data class FUpNext(
 )
 
 data class FUpNextSchedule(
+    val s: Int? = null,
     val r: Int? = null,
     val name: String? = null,
     val date: String? = null,
     val time: String? = null,
     val flag: String? = null,
-    val circuit: String? = null
+    val circuit: String? = null,
+    val circuitName: String? = null
 )
--- app-repo/src/main/java/tmg/flashback/repo/models/remoteconfig/UpNextSchedule.kt
+++ app-repo/src/main/java/tmg/flashback/repo/models/remoteconfig/UpNextSchedule.kt
@@ -4,10 +4,12 @@ import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalTime
 
 data class UpNextSchedule(
+    val season: Int,
     val round: Int,
     val name: String,
     val date: LocalDate,
     val time: LocalTime?,
     val flag: String?,
-    val circuitId: String?
+    val circuitId: String?,
+    val circuitName: String?
 )
