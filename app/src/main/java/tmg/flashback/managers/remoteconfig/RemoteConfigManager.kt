6f3d54e388c3bf27ef112f5554282fec4a686b79
Jordan Fisher <thementalgoose@gmail.com>
2021-01-06 22:53:31 +0000

    Added default indicator season list

--- app/src/main/java/tmg/flashback/managers/remoteconfig/RemoteConfigManager.kt
+++ app/src/main/java/tmg/flashback/managers/remoteconfig/RemoteConfigManager.kt
@@ -5,6 +5,9 @@ interface RemoteConfigManager {
     suspend fun update(andActivate: Boolean = false): Boolean
     suspend fun activate(): Boolean
 
+    /**
+     * Determine if the app version has changed in a way which
+     *   requires a remote sync before loading into the app
+     */
     val requiresRemoteSync: Boolean
-    fun setRemoteSyncPerformed()
 }
