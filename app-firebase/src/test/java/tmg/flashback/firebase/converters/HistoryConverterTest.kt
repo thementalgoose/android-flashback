4e1dd3244c0197591858c73849072e76e982f28a
Jordan Fisher <thementalgoose@gmail.com>
2021-01-09 20:48:42 +0000

    Added converter test to cover has qualifying mapping

---
+++ app-firebase/src/test/java/tmg/flashback/firebase/converters/HistoryConverterTest.kt
@@ -0,0 +1,16 @@
+package tmg.flashback.firebase.converters
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import tmg.flashback.firebase.currentYear
+import tmg.flashback.firebase.testutils.BaseTest
+
+internal class HistoryConverterTest: BaseTest() {
+
+    @Test
+    fun `HistoryConverter all data up too is one less than this year when all data confirmed`() {
+
+        assertEquals(currentYear - 1, allDataUpToo)
+    }
+
+}
