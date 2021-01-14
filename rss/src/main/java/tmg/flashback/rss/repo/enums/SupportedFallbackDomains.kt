40064c6f5437e19dc3295f5a70c525330f310001
Jordan Fisher <thementalgoose@gmail.com>
2021-01-14 23:20:12 +0000

    Removed support article fallback

--- rss/src/main/java/tmg/flashback/rss/repo/enums/SupportedFallbackDomains.kt
+++
@@ -1,6 +0,0 @@
-package tmg.flashback.rss.repo.enums
-
-//val supportedFallbackDomains = mapOf(
-//        "bbc.co.uk" to SupportedArticleSource.BBC_SPORT,
-//        "f1i.com" to SupportedArticleSource.F1I
-//)
