8ce5d4ae848d2dc45ca8bf8a5dd4caaeb2135519
Jordan Fisher <thementalgoose@gmail.com>
2021-01-27 22:52:40 +0000

    Removed comments from firebase error handler

--- app-firebase/src/main/java/tmg/flashback/firebase/FirebaseRepo.kt
+++ app-firebase/src/main/java/tmg/flashback/firebase/FirebaseRepo.kt
@@ -105,19 +105,19 @@ open class FirebaseRepo(
             FirebaseFirestoreException.Code.CANCELLED -> { }
             FirebaseFirestoreException.Code.NOT_FOUND -> {
                 crashManager.logException(exception, "Accessing $path resulted in not found")
-            } // this.onError(DoesntExistError(debugData))
+            }
             FirebaseFirestoreException.Code.ALREADY_EXISTS -> {
                 crashManager.logException(exception, "Already exists accessing $path")
-            } // this.onError(RecordAlreadyExistsError(debugData))
+            }
             FirebaseFirestoreException.Code.PERMISSION_DENIED -> {
                 crashManager.logException(exception, "Permission denied while accessing $path")
-            } // this.onError(PermissionError(debugData))
+            }
             FirebaseFirestoreException.Code.UNAUTHENTICATED -> {
                 crashManager.logException(exception, "Unauthenticated while accessing $path")
-            } // this.onError(UnauthenticatedError(debugData))
+            }
             else -> {
                 crashManager.logException(exception, "Unsupported error thrown by Firebase $path")
-            } // this.onError(Error("Error occurred ${exception.code}"))
+            }
         }
     }
 }
