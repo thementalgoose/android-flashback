0cf93a48e499df26f5ecee11da1a11488a8ed26c
Jordan Fisher <thementalgoose@gmail.com>
2021-01-31 00:20:11 +0000

    Updated unit tests for data-firebase module

--- data-firebase/src/test/java/tmg/flashback/firebase/extensions/StringExtensionsKtTest.kt
+++ data-firebase/src/test/java/tmg/flashback/firebase/extensions/StringExtensionsKtTest.kt
@@ -5,8 +5,26 @@ import org.junit.jupiter.api.Test
 
 internal class StringExtensionsKtTest {
 
+    data class TestPojo(
+        val str: String,
+        val int: Int,
+        val bool: Boolean,
+        val nullable: String? = null,
+        val list: List<String>
+    )
+
     @Test
-    fun `toJson implement this!`() {
-        TODO("Implement this")
+    fun `toJson converts a sample object to expected JSON`() {
+
+        val expectedJson = """{"str":"test","int":2,"bool":true,"nullable":null,"list":["item-1"]}""".trimIndent()
+        val exampleModel = TestPojo(
+            str = "test",
+            int = 2,
+            bool = true,
+            nullable = null,
+            list = listOf("item-1")
+        )
+
+        assertEquals(exampleModel, expectedJson.toJson<TestPojo>())
     }
 }
