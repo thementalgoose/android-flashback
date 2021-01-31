ab6b18c903e299d0c734d5a1d65075bd0071b511
Jordan Fisher <thementalgoose@gmail.com>
2021-01-31 17:58:42 +0000

    Increases test coverage of remote config conversion

--- app/src/test/java/tmg/flashback/managers/configuration/models/RemoteConfigAllSeasonsTest.kt
+++ app/src/test/java/tmg/flashback/managers/configuration/models/RemoteConfigAllSeasonsTest.kt
@@ -8,11 +8,23 @@ internal class RemoteConfigAllSeasonsTest: BaseTest() {
 
     @Test
     fun `convert ignores null s values`() {
-        TODO("Implement me")
+        val model = RemoteConfigAllSeasons(
+            seasons = listOf(
+                RemoteConfigAllSeason(null),
+                RemoteConfigAllSeason(2019)
+            )
+        )
+        assertEquals(setOf(2019), model.convert())
     }
 
     @Test
     fun `convert maps all items`() {
-        TODO("Implement me")
+        val model = RemoteConfigAllSeasons(
+            seasons = listOf(
+                RemoteConfigAllSeason(2017),
+                RemoteConfigAllSeason(2019)
+            )
+        )
+        assertEquals(setOf(2019, 2017), model.convert())
     }
 }
