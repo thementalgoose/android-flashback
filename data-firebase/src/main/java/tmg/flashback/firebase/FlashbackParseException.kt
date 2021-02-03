321c04ec337c06bb8ca141dd05c70f83d4a04c64
Jordan Fisher <thementalgoose@gmail.com>
2021-02-03 22:45:43 +0000

    Added java doccomments to methods in FirebaseRepo

--- data-firebase/src/main/java/tmg/flashback/firebase/FlashbackParseException.kt
+++ data-firebase/src/main/java/tmg/flashback/firebase/FlashbackParseException.kt
@@ -2,4 +2,7 @@ package tmg.flashback.firebase
 
 import java.lang.RuntimeException
 
+/**
+ * Exception that's used to wrap issues thrown when data is being parsed from firestore
+ */
 class FlashbackParseException(msg: String, cause: Throwable): RuntimeException(msg, cause)
