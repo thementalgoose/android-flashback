fc5901f1781a0b0c354ea88a694840aea5c7510d
Jordan Fisher <thementalgoose@gmail.com>
2021-02-21 20:17:23 +0000

    Moved Formula1 tests to statistics module

--- app-statistics/src/test/java/tmg/flashback/statistics/constants/Formula1Test.kt
+++ app-statistics/src/test/java/tmg/flashback/statistics/constants/Formula1Test.kt
@@ -1,23 +1,22 @@
-package tmg.flashback.constants
+package tmg.flashback.statistics.constants
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import tmg.flashback.constants.App.currentYear
-import tmg.flashback.statistics.constants.Formula1
-import tmg.flashback.testutils.BaseTest
+import org.threeten.bp.Year
+import tmg.flashback.statistics.testutils.BaseTest
 
 internal class Formula1Test: BaseTest() {
 
     @ParameterizedTest(name = "Max points in {0} season is {1}")
     @CsvSource(
-            "2020,25",
-            "2010,25",
-            "2009,10",
-            "1991,10",
-            "1990,8",
-            "1950,8"
+        "2020,25",
+        "2010,25",
+        "2009,10",
+        "1991,10",
+        "1990,8",
+        "1950,8"
     )
     fun `maxPoints by season returns correct amount of points`(season: Int, expectedPoints: Int) {
 
@@ -27,6 +26,6 @@ internal class Formula1Test: BaseTest() {
     @Test
     fun `maxPoints by current year returns correct amounts of points`() {
 
-        assertEquals(25, Formula1.maxPointsBySeason(currentYear))
+        assertEquals(25, Formula1.maxPointsBySeason(Year.now().value))
     }
 }
