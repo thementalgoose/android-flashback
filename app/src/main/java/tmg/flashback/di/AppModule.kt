7c3079b7c6927165d9c1f6b0dfabdd6176089355
Jordan Fisher <thementalgoose@gmail.com>
2021-03-04 22:38:56 +0000

    Updates app module to using view binding

--- app/src/main/java/tmg/flashback/di/AppModule.kt
+++ app/src/main/java/tmg/flashback/di/AppModule.kt
@@ -38,7 +38,7 @@ import tmg.flashback.statistics.ui.overview.constructor.ConstructorViewModel
 import tmg.flashback.statistics.ui.overview.driver.DriverViewModel
 import tmg.flashback.statistics.ui.overview.driver.season.DriverSeasonViewModel
 import tmg.flashback.statistics.ui.race.RaceViewModel
-import tmg.flashback.statistics.ui.settings.SettingsAllViewModel
+import tmg.flashback.ui.settings.SettingsAllViewModel
 import tmg.flashback.ui.settings.about.SettingsAboutViewModel
 import tmg.flashback.ui.settings.customisation.SettingsCustomisationViewModel
 import tmg.flashback.ui.settings.customisation.animation.AnimationSpeedViewModel
--- app/src/main/java/tmg/flashback/ui/settings/customisation/animation/AnimationSpeedBottomSheetFragment.kt
+++ app/src/main/java/tmg/flashback/ui/settings/customisation/animation/AnimationSpeedBottomSheetFragment.kt
@@ -1,24 +1,26 @@
 package tmg.flashback.ui.settings.customisation.animation
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.fragment_bottom_sheet_theme.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import tmg.flashback.R
 import tmg.flashback.core.enums.AnimationSpeed
 import tmg.flashback.core.ui.BaseBottomSheetFragment
 import tmg.flashback.core.ui.bottomsheet.BottomSheetAdapter
+import tmg.flashback.databinding.FragmentBottomSheetAnimationSpeedBinding
 import tmg.utilities.extensions.observe
 import tmg.utilities.extensions.observeEvent
 
-class AnimationSpeedBottomSheetFragment: BaseBottomSheetFragment() {
+class AnimationSpeedBottomSheetFragment: BaseBottomSheetFragment<FragmentBottomSheetAnimationSpeedBinding>() {
 
     private val viewModel: AnimationSpeedViewModel by viewModel()
 
     private lateinit var adapter: BottomSheetAdapter
 
-    override fun layoutId() = R.layout.fragment_bottom_sheet_animation_speed
+    override fun inflateView(inflater: LayoutInflater) =
+        FragmentBottomSheetAnimationSpeedBinding.inflate(inflater)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,8 +30,8 @@ class AnimationSpeedBottomSheetFragment: BaseBottomSheetFragment() {
                     viewModel.inputs.selectAnimationSpeed(AnimationSpeed.values()[it.id])
                 }
         )
-        list.adapter = adapter
-        list.layoutManager = LinearLayoutManager(context)
+        binding.list.adapter = adapter
+        binding.list.layoutManager = LinearLayoutManager(context)
 
         observe(viewModel.outputs.animationSpeedPreference) {
             adapter.list = it
