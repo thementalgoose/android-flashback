acf0822c8c77eeaa5cbccf771ff0b5ab3d29c886
Jordan Fisher <thementalgoose@gmail.com>
2021-04-01 23:42:03 +0100

    WIP Adding multiple values for race up next times for menu

---
+++ app-statistics/src/main/java/tmg/flashback/statistics/ui/shared/timelist/viewholders/TimeViewHolder.kt
@@ -0,0 +1,135 @@
+package tmg.flashback.statistics.ui.shared.timelist.viewholders
+
+import androidx.recyclerview.widget.RecyclerView
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalTime
+import org.threeten.bp.format.DateTimeFormatter
+import tmg.flashback.core.model.UpNextScheduleTimestamp
+import tmg.flashback.data.utils.daysBetween
+import tmg.flashback.data.utils.hoursAndMins
+import tmg.flashback.data.utils.secondsBetween
+import tmg.flashback.statistics.R
+import tmg.flashback.statistics.databinding.ViewSeasonListUpNextBinding
+import tmg.flashback.statistics.databinding.ViewSeasonListUpNextTimelistBinding
+import tmg.flashback.statistics.ui.shared.timelist.TimeListDisplayType
+import tmg.flashback.statistics.ui.shared.timelist.TimeListItem
+import tmg.utilities.extensions.fromHtml
+import tmg.utilities.extensions.ordinalAbbreviation
+import tmg.utilities.extensions.views.context
+import tmg.utilities.extensions.views.getString
+
+class TimeViewHolder(
+    private val binding: ViewSeasonListUpNextTimelistBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: TimeListItem) {
+        binding.title.text = item.item.label
+
+        item.item.timestamp.ifDate { date ->
+            when (item.type) {
+                TimeListDisplayType.RELATIVE -> {
+                    val days = daysBetween(LocalDate.now(), date)
+                    if (date == LocalDate.now()) {
+                        binding.dates.text = getString(R.string.dashboard_up_next_date_today)
+                    } else {
+                        binding.dates.text = context.resources.getQuantityString(
+                            R.plurals.dashboard_up_next_suffix_days,
+                            days,
+                            days
+                        )
+                    }
+                }
+                else -> {
+                    val ordinal = date.dayOfMonth.ordinalAbbreviation
+                    binding.dates.text = date.format(DateTimeFormatter.ofPattern("d'${ordinal}' MMM"))
+                }
+            }
+        }
+
+        item.item.timestamp.ifDateAndTime { utc, local ->
+            when (item.type) {
+                TimeListDisplayType.UTC -> {
+                    val ordinal = utc.dayOfMonth.ordinalAbbreviation
+                    val date = utc.format(DateTimeFormatter.ofPattern("d'${ordinal} MMM"))
+                    val time = utc.format(DateTimeFormatter.ofPattern("HH:mm"))
+                    binding.dates.text = "$date at $time"
+                }
+                TimeListDisplayType.LOCAL -> {
+                    val ordinal = local.dayOfMonth.ordinalAbbreviation
+                    val date = local.format(DateTimeFormatter.ofPattern("d'${ordinal} MMM"))
+                    val time = local.format(DateTimeFormatter.ofPattern("HH:mm"))
+                    binding.dates.text = "$date at $time"
+                }
+                TimeListDisplayType.RELATIVE -> {
+                    val localDate = local.toLocalDate()
+                    val localTime = local.toLocalTime()
+                    val nowTime = LocalTime.now()
+                    val nowDate = LocalDate.now()
+
+                    if (localDate > nowDate) {
+                        val days = daysBetween(nowDate, localDate)
+                        if (localDate == nowDate) {
+                            binding.dates.text = getString(R.string.dashboard_up_next_date_today)
+                        } else {
+                            binding.dates.text = context.resources.getQuantityString(
+                                R.plurals.dashboard_up_next_suffix_days,
+                                days,
+                                days
+                            )
+                        }
+                    }
+                    else if (localTime >= nowTime) {
+                        val (hours, minutes) = secondsBetween(nowTime, localTime).hoursAndMins
+                        when {
+                            hours > 12 -> {
+                                binding.dates.text =
+                                    getString(R.string.dashboard_up_next_datetime_hour, hours)
+                            }
+                            hours > 0 -> {
+                                binding.dates.text = getString(
+                                    R.string.dashboard_up_next_datetime_hour_min,
+                                    hours,
+                                    minutes
+                                )
+                            }
+                            minutes > 0 -> {
+                                binding.dates.text = context.resources.getQuantityString(
+                                    R.plurals.dashboard_up_next_datetime_mins,
+                                    minutes,
+                                    minutes
+                                )
+                            }
+                            else -> {
+                                binding.dates.text =
+                                    getString(R.string.dashboard_up_next_datetime_now)
+                            }
+                        }
+                    }
+                    else {
+                        val (hoursSinceStart, minutesSinceStart) = secondsBetween(
+                            localTime,
+                            nowTime
+                        ).hoursAndMins
+
+                        when {
+                            hoursSinceStart > 0 -> {
+                                binding.dates.text = context.resources.getQuantityString(
+                                    R.plurals.dashboard_up_next_datetime_started_hour,
+                                    hoursSinceStart,
+                                    hoursSinceStart
+                                )
+                            }
+                            else -> {
+                                binding.dates.text = context.resources.getQuantityString(
+                                    R.plurals.dashboard_up_next_datetime_started_mins,
+                                    minutesSinceStart,
+                                    minutesSinceStart
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
