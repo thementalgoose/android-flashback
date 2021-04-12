b463c294206bb40436a7a952cf720fd11c935883
Jordan Fisher <thementalgoose@gmail.com>
2021-04-12 20:12:24 +0100

    Added test coverage for date utils tests

---
+++ app-statistics/src/test/java/tmg/flashback/statistics/ui/util/DateUtilsKtTest.kt
@@ -0,0 +1,26 @@
+package tmg.flashback.statistics.ui.util
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.threeten.bp.LocalDate
+import org.threeten.bp.format.DateTimeFormatter
+import tmg.flashback.statistics.testutils.BaseTest
+
+internal class DateUtilsTest: BaseTest() {
+
+    @ParameterizedTest(name = "Born on {0} with now being {1} means person is {2} years old")
+    @CsvSource(
+        "1995-01-01,2021-01-01,26",
+        "2010-01-01,2010-01-01,0",
+        "2010-01-01,2010-12-31,0",
+        "2010-01-01,2011-01-01,1",
+        "1969-01-01,2011-01-02,42"
+    )
+    fun `date age calculation shows correct age`(birthday: String, now: String, expectedAge: Int) {
+        val birthdayDate = LocalDate.parse(birthday, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        val nowDate = LocalDate.parse(now, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+
+        assertEquals(expectedAge, birthdayDate.age(nowDate))
+    }
+}
