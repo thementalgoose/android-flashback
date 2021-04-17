35ba486ac4ccd56f17a3b6dd6ee6e21b9cf050d5
Jordan Fisher <thementalgoose@gmail.com>
2021-04-17 21:43:42 +0100

    Fixed settings screen settings link that bypasses settings screen

--- app-rss/src/main/java/tmg/flashback/rss/ui/RSSFragment.kt
+++ app-rss/src/main/java/tmg/flashback/rss/ui/RSSFragment.kt
@@ -44,7 +44,7 @@ class RSSFragment: BaseFragment<FragmentRssBinding>() {
 
         adapter = RSSAdapter(
             openConfigure = {
-                openConfigure()
+                openConfigure(InitialScreen.CONFIGURE)
             },
             articleClicked = { article, _ ->
                 if (repository.newsOpenInExternalBrowser) {
@@ -67,7 +67,7 @@ class RSSFragment: BaseFragment<FragmentRssBinding>() {
         }
 
         binding.settings.setOnClickListener {
-            openConfigure()
+            openConfigure(InitialScreen.SETTINGS)
         }
 
         observe(viewModel.outputs.list) {
@@ -95,10 +95,10 @@ class RSSFragment: BaseFragment<FragmentRssBinding>() {
         }
     }
 
-    private fun openConfigure() {
+    private fun openConfigure(screen: InitialScreen) {
         activity?.let {
             startActivityForResult(
-                RSSSettingsActivity.intent(it, InitialScreen.CONFIGURE),
+                RSSSettingsActivity.intent(it, screen),
                 1001
             )
         }
