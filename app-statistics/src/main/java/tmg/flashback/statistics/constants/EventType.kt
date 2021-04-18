ba95c7471b4ffcaade341b4693bdc03f10955314
Jordan Fisher <thementalgoose@gmail.com>
2021-04-18 22:51:30 +0100

    Adds Calendar as navigation type on Dashboard

--- app-statistics/src/main/java/tmg/flashback/statistics/constants/EventType.kt
+++ app-statistics/src/main/java/tmg/flashback/statistics/constants/EventType.kt
@@ -9,6 +9,7 @@ enum class ViewType(
     DRIVER_SEASON("view_driver_season"),
     CONSTRUCTOR("view_constructor"),
     CIRCUIT("view_circuit"),
+    DASHBOARD_SEASON_CALENDAR("view_season_calendar"),
     DASHBOARD_SEASON_SCHEDULE("view_season_schedule"),
     DASHBOARD_SEASON_DRIVER("view_season_driver"),
     DASHBOARD_SEASON_CONSTRUCTOR("view_season_constructor"),
