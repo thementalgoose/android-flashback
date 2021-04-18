00932e39f399786e5b4f0f7b1e2b42463242d90b
Jordan Fisher <thementalgoose@gmail.com>
2021-04-18 18:04:37 +0100

    Updated privacy policy to use motion layout

--- app/src/main/java/tmg/flashback/ui/settings/privacy/PrivacyPolicyActivity.kt
+++ app/src/main/java/tmg/flashback/ui/settings/privacy/PrivacyPolicyActivity.kt
@@ -15,30 +15,12 @@ import tmg.utilities.extensions.observeEvent
 class PrivacyPolicyActivity: BaseActivity() {
 
     private lateinit var binding: ActivityPrivacyPolicyBinding
-    private val viewModel: PrivacyPolicyViewModel by viewModel()
 
-    override val screenAnalytics = ScreenAnalytics(
-        screenName = "Privacy policy"
-    )
+    override val screenAnalytics: ScreenAnalytics? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPrivacyPolicyBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        analyticsController.logEvent(ViewType.SETTINGS_PRIVACY_POLICY)
-
-        binding.header.text = getString(R.string.privacy_policy_title)
-
-        binding.tvPolicy.text = getString(R.string.privacy_policy_data).fromHtml()
-        binding.tvPolicy.movementMethod = LinkMovementMethod.getInstance()
-
-        binding.back.setOnClickListener {
-            viewModel.inputs.clickBack()
-        }
-
-        observeEvent(viewModel.outputs.goBack) {
-            onBackPressed()
-        }
     }
 }
