cf869150f82f3ecec02a18044952ac0a7d7c51bc
Jordan Fisher <thementalgoose@gmail.com>
2021-04-19 20:33:16 +0100

    Updates layout to highlight race in the calendar view

--- app-statistics/src/main/java/tmg/flashback/statistics/ui/dashboard/season/viewholders/CalendarViewHolders.kt
+++ app-statistics/src/main/java/tmg/flashback/statistics/ui/dashboard/season/viewholders/CalendarViewHolders.kt
@@ -5,17 +5,20 @@ import androidx.recyclerview.widget.RecyclerView
 import org.threeten.bp.DayOfWeek
 import org.threeten.bp.LocalDate
 import org.threeten.bp.format.TextStyle
+import org.threeten.bp.temporal.TemporalAdjusters
 import tmg.flashback.statistics.databinding.LayoutDashboardSeasonCalendarWeekBinding
 import tmg.flashback.statistics.databinding.ViewDashboardSeasonCalendarHeaderBinding
 import tmg.flashback.statistics.databinding.ViewDashboardSeasonCalendarMonthBinding
 import tmg.flashback.statistics.databinding.ViewDashboardSeasonCalendarWeekBinding
 import tmg.flashback.statistics.ui.dashboard.season.SeasonItem
+import tmg.flashback.statistics.ui.util.getFlagResourceAlpha3
+import tmg.utilities.extensions.views.context
 import tmg.utilities.extensions.views.show
 import java.util.*
 
 class CalendarHeaderViewHolder(
     private val binding: ViewDashboardSeasonCalendarHeaderBinding
-): RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(binding.root) {
 
     val cells: List<TextView> by lazy {
         return@lazy listOf(
@@ -38,16 +41,17 @@ class CalendarHeaderViewHolder(
 
 class CalendarMonthViewHolder(
     private val binding: ViewDashboardSeasonCalendarMonthBinding
-): RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: SeasonItem.CalendarMonth) {
-        binding.month.text = item.month.getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault())
+        binding.month.text =
+            item.month.getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault())
     }
 }
 
 class CalendarWeekViewHolder(
     private val binding: ViewDashboardSeasonCalendarWeekBinding
-): RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(binding.root) {
 
     val cells: List<LayoutDashboardSeasonCalendarWeekBinding> by lazy {
         return@lazy listOf(
@@ -63,6 +67,8 @@ class CalendarWeekViewHolder(
 
     fun bind(item: SeasonItem.CalendarWeek) {
 
+        binding.highlight.show(item.race != null)
+
         cells.forEach {
             it.day.text = ""
         }
@@ -70,12 +76,48 @@ class CalendarWeekViewHolder(
         val startingIndex = item.startingDay.dayOfWeek.value - 1
 
         val dayOfMonth = item.startingDay.dayOfMonth
-        val lastDayOfMonth: Int = item.startingDay.withDayOfMonth(item.startingDay.lengthOfMonth()).dayOfMonth
+        val lastDayOfMonth: LocalDate =
+            item.startingDay.withDayOfMonth(item.startingDay.lengthOfMonth())
+
+        var lastSimulatedDay = 0
 
         for ((offset, x) in (startingIndex until cells.size).withIndex()) {
+
             val day = (dayOfMonth + offset)
             cells[x].day.text = day.toString()
-            cells[x].day.show(day <= lastDayOfMonth)
+            cells[x].day.show(day <= lastDayOfMonth.dayOfMonth)
+
+            val date = item.startingDay.plusDays(offset.toLong())
+            if (LocalDate.now() > date) {
+                cells[x].day.alpha = textAlpha
+            } else {
+                cells[x].day.alpha = 1.0f
+            }
+
+            lastSimulatedDay = day
         }
+
+        if (lastSimulatedDay > lastDayOfMonth.dayOfMonth || item.race == null) {
+            binding.highlight.alpha = alphaHighlightEnabled
+            binding.highlight.show(false)
+            binding.flag.show(false)
+        } else {
+            if (item.race.date < LocalDate.now()) {
+                binding.highlight.alpha = alphaHighlightDisabled
+            }
+            else {
+                binding.highlight.alpha = alphaHighlightEnabled
+            }
+            binding.highlight.show(true)
+            binding.flag.show(true)
+            binding.flag.setImageResource(context.getFlagResourceAlpha3(item.race.countryISO))
+        }
+    }
+
+    companion object {
+        private const val alphaHighlightEnabled = 0.6f
+        private const val alphaHighlightDisabled = 0.2f
+
+        private const val textAlpha = 0.35f
     }
 }
