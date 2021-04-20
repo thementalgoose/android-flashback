285f931e89f190bd33c394fa7abae517d1f2186a
Jordan Fisher <thementalgoose@gmail.com>
2021-04-20 19:52:23 +0100

    Separated view holders to separate files for calendar view

--- app-statistics/src/main/java/tmg/flashback/statistics/ui/dashboard/season/viewholders/CalendarWeekViewHolder.kt
+++ app-statistics/src/main/java/tmg/flashback/statistics/ui/dashboard/season/viewholders/CalendarWeekViewHolder.kt
@@ -1,53 +1,13 @@
 package tmg.flashback.statistics.ui.dashboard.season.viewholders
 
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import org.threeten.bp.DayOfWeek
 import org.threeten.bp.LocalDate
-import org.threeten.bp.format.TextStyle
-import org.threeten.bp.temporal.TemporalAdjusters
 import tmg.flashback.statistics.databinding.LayoutDashboardSeasonCalendarWeekBinding
-import tmg.flashback.statistics.databinding.ViewDashboardSeasonCalendarHeaderBinding
-import tmg.flashback.statistics.databinding.ViewDashboardSeasonCalendarMonthBinding
 import tmg.flashback.statistics.databinding.ViewDashboardSeasonCalendarWeekBinding
 import tmg.flashback.statistics.ui.dashboard.season.SeasonItem
 import tmg.flashback.statistics.ui.util.getFlagResourceAlpha3
 import tmg.utilities.extensions.views.context
 import tmg.utilities.extensions.views.show
-import java.util.*
-
-class CalendarHeaderViewHolder(
-    private val binding: ViewDashboardSeasonCalendarHeaderBinding
-) : RecyclerView.ViewHolder(binding.root) {
-
-    val cells: List<TextView> by lazy {
-        return@lazy listOf(
-            binding.cell1,
-            binding.cell2,
-            binding.cell3,
-            binding.cell4,
-            binding.cell5,
-            binding.cell6,
-            binding.cell7
-        )
-    }
-
-    init {
-        DayOfWeek.values().forEachIndexed { index, dayOfWeek ->
-            cells[index].text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
-        }
-    }
-}
-
-class CalendarMonthViewHolder(
-    private val binding: ViewDashboardSeasonCalendarMonthBinding
-) : RecyclerView.ViewHolder(binding.root) {
-
-    fun bind(item: SeasonItem.CalendarMonth) {
-        binding.month.text =
-            item.month.getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault())
-    }
-}
 
 class CalendarWeekViewHolder(
     private val binding: ViewDashboardSeasonCalendarWeekBinding
