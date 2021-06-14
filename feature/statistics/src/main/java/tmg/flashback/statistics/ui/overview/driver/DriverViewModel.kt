0e03ae0bd6968f7aefe8d7e36a9702d760277475
Jordan Fisher <thementalgoose@gmail.com>
2021-06-14 13:11:57 +0100

    Fixed compile errors

--- feature/statistics/src/main/java/tmg/flashback/statistics/ui/overview/driver/DriverViewModel.kt
+++ feature/statistics/src/main/java/tmg/flashback/statistics/ui/overview/driver/DriverViewModel.kt
@@ -79,7 +79,7 @@ class DriverViewModel(
                     ))
 
                     if (it.hasChampionshipCurrentlyInProgress) {
-                        val latestRound = it.standings.maxBy { it.season }?.raceOverview?.maxBy { it.round }
+                        val latestRound = it.standings.maxByOrNull { it.season }?.raceOverview?.maxByOrNull { it.round }
                         if (latestRound != null) {
                             list.add(DriverSummaryItem.ErrorItem(SyncDataItem.MessageRes(R.string.results_accurate_for_year, listOf(latestRound.season, latestRound.raceName, latestRound.round))))
                         }
