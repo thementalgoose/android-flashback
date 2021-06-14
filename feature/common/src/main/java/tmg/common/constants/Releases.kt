ef4c0cc60ef5128c797f5f14bbc0d37dd23b8f56
Jordan Fisher <thementalgoose@gmail.com>
2021-06-14 13:16:23 +0100

    Bumped version to 6.1.3 (71)

--- feature/common/src/main/java/tmg/common/constants/Releases.kt
+++ feature/common/src/main/java/tmg/common/constants/Releases.kt
@@ -45,5 +45,6 @@ enum class Releases(
         VERSION_53(version = 53, isMajor = false, release = R.string.release_53),
         VERSION_54(version = 54, isMajor = false, release = R.string.release_54),
         VERSION_57(version = 57, isMajor = false, release = R.string.release_57),
-        VERSION_70(version = 70, isMajor = true, release = R.string.release_70);
+        VERSION_70(version = 70, isMajor = true, release = R.string.release_70),
+        VERSION_71(version = 71, isMajor = false, release = R.string.release_71);
 }
