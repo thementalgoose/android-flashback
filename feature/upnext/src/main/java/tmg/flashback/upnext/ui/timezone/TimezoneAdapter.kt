ccc1c1288d01eb304bf2d4eb9c93311446143617
Jordan Fisher <thementalgoose@gmail.com>
2021-06-17 21:38:35 +0100

    Adds timezone pill adapter with stubbed data

---
+++ feature/upnext/src/main/java/tmg/flashback/upnext/ui/timezone/TimezoneAdapter.kt
@@ -0,0 +1,28 @@
+package tmg.flashback.upnext.ui.timezone
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import tmg.flashback.upnext.databinding.ViewTimezoneBinding
+
+class TimezoneAdapter: RecyclerView.Adapter<TimezoneViewHolder>() {
+
+    var list: List<TimezoneItem> = listOf(TimezoneItem("Your time"), TimezoneItem("Europe/London"))
+        @SuppressLint("NotifyDataSetChanged")
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimezoneViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return TimezoneViewHolder(ViewTimezoneBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: TimezoneViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount() = list.size
+}
