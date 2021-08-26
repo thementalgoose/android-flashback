7254bfdc2530fa2df58af97ea034d57c5a79dc39
Jordan Fisher <thementalgoose@gmail.com>
2021-08-26 23:11:49 +0100

    Adds unit tests for SyncViewModel and HomeViewModel

---
+++ app/src/test/java/tmg/flashback/ui/dashboard/HomeViewModelTest.kt
@@ -0,0 +1,93 @@
+package tmg.flashback.ui.dashboard
+
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import tmg.common.controllers.ForceUpgradeController
+import tmg.configuration.controllers.ConfigController
+import tmg.flashback.managers.appshortcuts.AppShortcutManager
+import tmg.flashback.rss.controllers.RSSController
+import tmg.testutils.BaseTest
+
+internal class HomeViewModelTest: BaseTest() {
+
+    private var mockAppShortcutManager: AppShortcutManager = mockk(relaxed = true)
+    private var mockRssController: RSSController = mockk(relaxed = true)
+    private var mockConfigurationManager: ConfigController = mockk(relaxed = true)
+    private var mockForceUpgradeController: ForceUpgradeController = mockk(relaxed = true)
+
+    private lateinit var sut: HomeViewModel
+
+    @BeforeEach
+    internal fun setUp() {
+        every { mockAppShortcutManager.enable() } returns true
+        every { mockAppShortcutManager.disable() } returns true
+        coEvery { mockConfigurationManager.applyPending() } returns true
+        every { mockConfigurationManager.requireSynchronisation } returns false
+        every { mockForceUpgradeController.shouldForceUpgrade } returns false
+        every { mockRssController.enabled } returns false
+    }
+
+    private fun initSUT() {
+        sut = HomeViewModel(mockConfigurationManager, mockRssController, mockForceUpgradeController, mockAppShortcutManager)
+    }
+
+    @Test
+    fun `initialise with config requiring sync notifies require sync`() = coroutineTest {
+        every { mockConfigurationManager.requireSynchronisation } returns true
+
+        initSUT()
+        sut.initialise()
+
+        assertTrue(sut.requiresSync)
+        assertFalse(sut.forceUpgrade)
+        assertFalse(sut.appliedChanges)
+    }
+
+    @Test
+    fun `initialise with force upgrade true notifies force upgrade`() = coroutineTest {
+        every { mockForceUpgradeController.shouldForceUpgrade } returns true
+
+        initSUT()
+        sut.initialise()
+
+        assertFalse(sut.requiresSync)
+        assertTrue(sut.forceUpgrade)
+        assertFalse(sut.appliedChanges)
+    }
+
+    @Test
+    fun `initialise with no force upgrade or sync with rss disabled notifies applied changes`() = coroutineTest {
+        every { mockRssController.enabled } returns false
+
+        initSUT()
+        sut.initialise()
+
+        coVerify { mockConfigurationManager.applyPending() }
+        verify { mockAppShortcutManager.disable() }
+
+        assertFalse(sut.requiresSync)
+        assertFalse(sut.forceUpgrade)
+        assertTrue(sut.appliedChanges)
+    }
+
+    @Test
+    fun `initialise with no force upgrade or sync with rss enabled notifies applied changes`() = coroutineTest {
+        every { mockRssController.enabled } returns true
+
+        initSUT()
+        sut.initialise()
+
+        coVerify { mockConfigurationManager.applyPending() }
+        verify { mockAppShortcutManager.enable() }
+
+        assertFalse(sut.requiresSync)
+        assertFalse(sut.forceUpgrade)
+        assertTrue(sut.appliedChanges)
+    }
+}
