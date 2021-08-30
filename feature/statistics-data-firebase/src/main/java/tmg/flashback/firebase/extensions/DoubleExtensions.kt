ed245f859cd4d1e1ec6b2f5acf6485a0338f847d
Jordan Fisher <thementalgoose@gmail.com>
2021-08-30 11:13:35 +0100

    Fixed points display extensions

--- feature/statistics-data-firebase/src/main/java/tmg/flashback/firebase/extensions/DoubleExtensions.kt
+++ feature/statistics-data-firebase/src/main/java/tmg/flashback/firebase/extensions/DoubleExtensions.kt
@@ -2,19 +2,26 @@ package tmg.flashback.firebase.extensions
 
 import java.math.RoundingMode
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import kotlin.math.roundToInt
 
 /**
  * Print points as a legible string
  */
 fun Double.pointsDisplay(): String {
-    val df = DecimalFormat("#.##")
+    val formatter = DecimalFormatSymbols.getInstance()
+    formatter.decimalSeparator = '.'
+    formatter.monetaryDecimalSeparator = '.'
+    val df = DecimalFormat("#.#")
     df.roundingMode = RoundingMode.CEILING
-    val result = df.format(this)
+    var result = df.format(this)
+    if (result.contains(',')) {
+        result = result.replace(',', '.')
+    }
     return if (result.endsWith(".5")) {
         result
     }
     else {
-        result.toDouble().roundToInt().toString()
+        result.toDoubleOrNull()?.roundToInt()?.toString() ?: this.toString()
     }
 }
