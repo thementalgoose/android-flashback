727cc1983aaaf464de0d8f42f14f53ee47f7f2ea
Jordan Fisher <thementalgoose@gmail.com>
2021-09-20 22:37:12 +0100

    Deletes unused files and extensions

--- app/src/main/java/tmg/flashback/extensions/CoroutineExtensions.kt
+++
@@ -1,9 +0,0 @@
-package tmg.flashback.extensions
-
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import tmg.utilities.lifecycle.DataEvent
-
-fun <T> Flow<T>.toDataEvent(): Flow<DataEvent<T>> {
-    return this.map { DataEvent(it) }
-}
