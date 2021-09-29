e50679072a549ea1eeeb0f5faf46ee14a9032971
Jordan Fisher <thementalgoose@gmail.com>
2021-09-29 21:52:23 +0100

    Adds ConstructorMapper and unit tests

--- feature/statistics-data-firebase/src/main/java/tmg/flashback/firebase/repos/ConstructorFirestore.kt
+++ feature/statistics-data-firebase/src/main/java/tmg/flashback/firebase/repos/ConstructorFirestore.kt
@@ -7,14 +7,18 @@ import tmg.flashback.firebase.converters.convert
 import tmg.flashback.firebase.models.FConstructorOverview
 import tmg.flashback.data.db.stats.ConstructorRepository
 import tmg.flashback.data.models.stats.ConstructorOverview
+import tmg.flashback.firebase.mappers.ConstructorMapper
 
 class ConstructorFirestore(
-        crashController: CrashController
+        crashController: CrashController,
+        private val constructorMapper: ConstructorMapper
 ) : FirebaseRepo(crashController), ConstructorRepository {
 
     override fun getConstructorOverview(constructorId: String): Flow<ConstructorOverview?> {
         crashController.log("document(constructors/$constructorId)")
         return document("constructors/$constructorId")
-                .getDoc<FConstructorOverview, ConstructorOverview> { it.convert() }
+                .getDoc<FConstructorOverview, ConstructorOverview> {
+                    constructorMapper.mapConstructorOverview(it)
+                }
     }
 }
