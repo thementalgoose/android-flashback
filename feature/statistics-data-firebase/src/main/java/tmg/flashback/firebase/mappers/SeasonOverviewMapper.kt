98cde5fe760846a3a65f485b0d7e013fdc2d8ec6
Jordan Fisher <thementalgoose@gmail.com>
2021-09-29 22:36:34 +0100

    Adds model values for FSeason values and blank mapper file

---
+++ feature/statistics-data-firebase/src/main/java/tmg/flashback/firebase/mappers/SeasonOverviewMapper.kt
@@ -0,0 +1,5 @@
+package tmg.flashback.firebase.mappers
+
+class SeasonOverviewMapper {
+
+}
--- feature/statistics-data-firebase/src/main/java/tmg/flashback/firebase/models/FSeason.kt
+++ feature/statistics-data-firebase/src/main/java/tmg/flashback/firebase/models/FSeason.kt
@@ -5,17 +5,23 @@ data class FSeason(
         val constructors: Map<String, FSeasonOverviewConstructor>? = null,
         val race: Map<String, FRound>? = null,
         val standings: FSeasonStatistics? = null
-)
+) {
+        companion object
+}
 
 data class FSeasonStatistics(
         val constructors: Map<String, FSeasonStatisticsPoints>? = null,
         val drivers: Map<String, FSeasonStatisticsPoints>? = null
-)
+) {
+        companion object
+}
 
 data class FSeasonStatisticsPoints(
         val p: Double? = 0.0,
         val pos: Int? = null
-)
+) {
+        companion object
+}
 
 data class FSeasonOverviewDriver(
         val id: String = "",
@@ -29,7 +35,9 @@ data class FSeasonOverviewDriver(
         val nationality: String = "",
         val nationalityISO: String = "",
         val constructorId: String = ""
-)
+) {
+        companion object
+}
 
 data class FSeasonOverviewConstructor(
         val id: String = "",
@@ -41,16 +49,6 @@ data class FSeasonOverviewConstructor(
 ) {
         companion object
 }
-
-data class FSeasonOverviewPoints(
-        val position: Int = -1,
-        val points: Double = -1.0
-)
-
-data class FSeasonOverviewPointsConfig(
-        val fastestLap: Int? = null,
-        val doubleForLastRace: Boolean? = null
-)
 
 data class FRound(
         val season: Int = -1,
@@ -64,7 +62,9 @@ data class FRound(
         val qualifying: Map<String, FSeasonOverviewRaceQualifying>? = null,
         val sprintQualifying: Map<String, FSeasonOverviewRaceSprintQualifying>? = null,
         val race: Map<String, FSeasonOverviewRaceRace>? = null
-)
+) {
+        companion object
+}
 
 data class FSeasonOverviewRaceCircuit(
         val id: String = "",
@@ -74,19 +74,25 @@ data class FSeasonOverviewRaceCircuit(
         val country: String = "",
         val countryISO: String = "",
         val location: FSeasonOverviewRaceCircuitLocation = FSeasonOverviewRaceCircuitLocation()
-)
+) {
+        companion object
+}
 
 data class FSeasonOverviewRaceCircuitLocation(
         val lat: String? = "",
         val lng: String? = ""
-)
+) {
+        companion object
+}
 
 data class FSeasonOverviewRaceQualifying(
         val pos: Int? = null,
         val q1: String? = null,
         val q2: String? = null,
         val q3: String? = null
-)
+) {
+        companion object
+}
 
 data class FSeasonOverviewRaceSprintQualifying(
         val result: Int? = null,
@@ -96,7 +102,9 @@ data class FSeasonOverviewRaceSprintQualifying(
         val status: String? = null,
         val points: Double? = null,
         val time: String? = null
-)
+) {
+        companion object
+}
 
 data class FSeasonOverviewRaceRace(
         val result: Int? = null,
@@ -107,10 +115,14 @@ data class FSeasonOverviewRaceRace(
         val points: Double? = null,
         val time: String? = null,
         val fastestLap: FSeasonOverviewRaceRaceFastestLap? = null
-)
+) {
+        companion object
+}
 
 data class FSeasonOverviewRaceRaceFastestLap(
         val pos: Int = -1,
         val lap: Int = -1,
         val time: String = ""
-)
+) {
+        companion object
+}
