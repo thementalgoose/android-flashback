730dadfb4353889992a555ad336114cc6dd02830
Jordan Fisher <thementalgoose@gmail.com>
2021-10-01 18:04:05 +0100

    Adds Watkins Glen circuit image

--- feature/formula1/src/main/java/tmg/flashback/formula1/enums/TrackLayout.kt
+++ feature/formula1/src/main/java/tmg/flashback/formula1/enums/TrackLayout.kt
@@ -62,6 +62,7 @@ enum class TrackLayout(
     TREMBLANT("tremblant", R.drawable.circuit_tremblant),
     VALENCIA("valencia", R.drawable.circuit_valencia),
     VILLENEUVE("villeneuve", R.drawable.circuit_villeneuve),
+    WATKINS_GLEN("watkins_glen", R.drawable.circuit_watkins_glen),
     YAS_MARINA("yas_marina", R.drawable.circuit_yas_marina),
     YEONGAM("yeongam", R.drawable.circuit_yeongam),
     ZANDVOORT("zandvoort", R.drawable.circuit_zandvoort);
