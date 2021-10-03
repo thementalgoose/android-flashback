8f63dac18099faf495ccd7cffc8abfa212f026fa
Jordan Fisher <thementalgoose@gmail.com>
2021-10-03 14:53:06 +0100

    Adds SearchController with enabled disabled flag and associated unit tests

---
+++ feature/statistics/src/main/java/tmg/flashback/statistics/controllers/SearchController.kt
@@ -0,0 +1,14 @@
+package tmg.flashback.statistics.controllers
+
+import tmg.flashback.statistics.repository.StatisticsRepository
+
+class SearchController(
+    private val statisticsRepository: StatisticsRepository
+) {
+
+    /**
+     * Is the search functionality enabled
+     */
+    val enabled: Boolean
+        get() = statisticsRepository.searchEnabled
+}
