d2237fd8b1a7019a6232aa8354faa32997e33437
Jordan Fisher <thementalgoose@gmail.com>
2021-10-25 18:39:45 +0100

    Updates home view model to set apply changes more often

--- app/src/main/java/tmg/flashback/di/AppModule.kt
+++ app/src/main/java/tmg/flashback/di/AppModule.kt
@@ -28,7 +28,7 @@ val appModule = module {
     viewModel { SettingsAllViewModel(get()) }
     viewModel { DashboardViewModel(get(), get(), get(), get(), get(), get()) }
     viewModel { ListViewModel(get(), get(), get()) }
-    viewModel { HomeViewModel(get(), get(), get(), get(), get()) }
+    viewModel { HomeViewModel(get(), get(), get(), get(), get(), get()) }
     viewModel { SyncViewModel(get(), get(), get(), get(), get()) }
 
     single { FlashbackStartup(get(), get(), get(), get(), get(), get()) }
