6f50583a2b5131f050d661169318abf45d2f1a6a
Jordan Fisher <thementalgoose@gmail.com>
2021-10-30 20:16:00 +0100

    Adds all retrofit methods that describe the API

---
+++ feature/stats/network/src/main/java/tmg/flashback/statistics/network/models/races/Round.kt
@@ -0,0 +1,12 @@
+package tmg.flashback.statistics.network.models.races
+
+import tmg.flashback.statistics.network.models.constructors.ConstructorData
+import tmg.flashback.statistics.network.models.drivers.DriverData
+
+data class Round(
+    val drivers: Map<String, DriverData>,
+    val constructors: Map<String, ConstructorData>,
+    val data: RaceData,
+    val race: Map<String, RaceResult>,
+    val qualifying: Map<String, QualifyingResult>
+)
