75a4ad2e0702ecf5d0f447a2fa1eb20432620f7d
Jordan Fisher <thementalgoose@gmail.com>
2021-10-31 23:56:34 +0000

    Removes CircuitFirestore

--- feature/stats/data/src/main/java/tmg/flashback/data/db/stats/CircuitRepository.kt
+++
@@ -1,8 +0,0 @@
-package tmg.flashback.data.db.stats
-
-import kotlinx.coroutines.flow.Flow
-import tmg.flashback.formula1.model.CircuitHistory
-
-interface CircuitRepository {
-    fun getCircuit(id: String): Flow<CircuitHistory?>
-}
