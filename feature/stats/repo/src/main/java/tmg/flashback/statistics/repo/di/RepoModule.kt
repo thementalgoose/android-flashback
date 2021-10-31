b96a94232d546af5e41e011a3c8d9333f2d2a4b2
Jordan Fisher <thementalgoose@gmail.com>
2021-10-31 19:27:51 +0000

    Added DI and removes methods to build the app

--- feature/stats/repo/src/main/java/tmg/flashback/statistics/repo/di/RepoModule.kt
+++ feature/stats/repo/src/main/java/tmg/flashback/statistics/repo/di/RepoModule.kt
@@ -1,20 +1,21 @@
 package tmg.flashback.statistics.repo.di
 
 import org.koin.dsl.module
+import tmg.flashback.statistics.network.di.networkModule
 import tmg.flashback.statistics.repo.CircuitRepository
 import tmg.flashback.statistics.repo.DriverRepository
 import tmg.flashback.statistics.repo.OverviewRepository
 import tmg.flashback.statistics.repo.mappers.app.CircuitMapper
-import tmg.flashback.statistics.repo.mappers.app.DriverMapper
 import tmg.flashback.statistics.repo.mappers.app.OverviewMapper
 import tmg.flashback.statistics.repo.mappers.network.NetworkCircuitMapper
 import tmg.flashback.statistics.repo.mappers.network.NetworkOverviewMapper
+import tmg.flashback.statistics.room.di.roomModule
 
-val repoModule = module {
+val repoModule = networkModule + roomModule + module {
 
     // Mappers
     single { CircuitMapper() }
-    single { DriverMapper() }
+//    single { DriverMapper() }
     single { OverviewMapper() }
     single { NetworkCircuitMapper() }
     single { NetworkOverviewMapper() }
