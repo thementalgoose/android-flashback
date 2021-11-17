ad4b0465cdc1ae2fcb41c95dcb1950df46a08bf4
Jordan Fisher <thementalgoose@gmail.com>
2021-11-17 17:57:55 +0000

    WIP adding inline view holder for times

---
+++ stats/statistics/src/main/java/tmg/flashback/statistics/ui/dashboard/schedule/viewholders/DayViewHolder.kt
@@ -0,0 +1,14 @@
+package tmg.flashback.statistics.ui.dashboard.schedule.viewholders
+
+import androidx.recyclerview.widget.RecyclerView
+import tmg.flashback.statistics.databinding.ViewInlineScheduleDayBinding
+import tmg.flashback.statistics.ui.dashboard.schedule.InlineSchedule
+
+class DayViewHolder(
+    binding: ViewInlineScheduleDayBinding
+): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: InlineSchedule.Day) {
+
+    }
+}
---
+++ stats/statistics/src/main/java/tmg/flashback/statistics/ui/dashboard/schedule/viewholders/ItemViewHolder.kt
@@ -0,0 +1,14 @@
+package tmg.flashback.statistics.ui.dashboard.schedule.viewholders
+
+import androidx.recyclerview.widget.RecyclerView
+import tmg.flashback.statistics.databinding.ViewInlineScheduleItemBinding
+import tmg.flashback.statistics.ui.dashboard.schedule.InlineSchedule
+
+class ItemViewHolder(
+    binding: ViewInlineScheduleItemBinding
+): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: InlineSchedule.Item) {
+
+    }
+}
