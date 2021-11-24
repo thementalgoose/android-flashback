ac83629c682fbabe06d33077c2a5f90f6c1cdc51
Jordan Fisher <thementalgoose@gmail.com>
2021-11-24 17:23:59 +0000

    Removes up next side panel fragment

--- stats/statistics/src/main/java/tmg/flashback/statistics/ui/dashboard/season/SeasonFragmentCallback.kt
+++ stats/statistics/src/main/java/tmg/flashback/statistics/ui/dashboard/season/SeasonFragmentCallback.kt
@@ -2,7 +2,6 @@ package tmg.flashback.statistics.ui.dashboard.season
 
 interface SeasonFragmentCallback {
      fun openMenu()
-     fun openNow()
      fun scrollUp()
      fun scrollDown()
      fun refresh()
--- stats/upnext/src/main/java/tmg/flashback/upnext/ui/dashboard/viewholder/DividerViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package tmg.flashback.upnext.ui.dashboard.viewholder
-
-import androidx.recyclerview.widget.RecyclerView
-import tmg.flashback.upnext.databinding.ViewBreakdownDividerBinding
-
-class DividerViewHolder(
-    private val binding: ViewBreakdownDividerBinding
-): RecyclerView.ViewHolder(binding.root)
