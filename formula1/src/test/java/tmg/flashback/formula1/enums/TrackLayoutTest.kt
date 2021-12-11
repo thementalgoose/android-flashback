d7bec0df739102626d00ff8c8d93a4ce610608e8
Jordan Fisher <thementalgoose@gmail.com>
2021-12-11 17:16:09 +0000

    Adds aintree, bermgarten, boavista, charade, dallas, detroit, pescara, port imperial, and riverside track images

--- formula1/src/test/java/tmg/flashback/formula1/enums/TrackLayoutTest.kt
+++ formula1/src/test/java/tmg/flashback/formula1/enums/TrackLayoutTest.kt
@@ -9,9 +9,23 @@ internal class TrackLayoutTest {
 
     @ParameterizedTest(name = "For the {0} {1} the {2} track layout will be used")
     @CsvSource(
-            "2020,Sakhir Grand Prix,SAKHIR"
+            "2020,Sakhir Grand Prix,SAKHIR",
+            ",Sakhir Grand Prix,BAHRAIN",
+            "2009,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2010,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2011,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2012,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2013,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2014,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2015,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2016,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2017,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2018,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2019,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            "2020,Abu Dhabi Grand Prix,YAS_MARINA_1",
+            ",Abu Dhabi Grand Prix,YAS_MARINA_2"
     )
-    fun `testing override values pull overriden enum`(year: Int, raceName: String, expected: TrackLayout) {
+    fun `testing override values pull overriden enum`(year: Int?, raceName: String, expected: TrackLayout) {
 
         val actual = TrackLayout.getTrack(SAKHIR.circuitId, year, raceName)
         assertEquals(expected, actual)
