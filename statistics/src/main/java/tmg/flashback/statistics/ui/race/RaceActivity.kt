e03c545cdb24847eb1359d241825abfd0adb32af
Jordan Fisher <thementalgoose@gmail.com>
2022-02-13 22:29:29 +0000

    Removed lateinit race data variable from RaceActivity

--- statistics/src/main/java/tmg/flashback/statistics/ui/race/RaceActivity.kt
+++ statistics/src/main/java/tmg/flashback/statistics/ui/race/RaceActivity.kt
@@ -3,16 +3,19 @@ package tmg.flashback.statistics.ui.race
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import org.koin.android.ext.android.inject
+import tmg.flashback.crash_reporting.controllers.CrashController
 import tmg.flashback.statistics.R
 import tmg.flashback.statistics.databinding.ActivityRaceBinding
 import tmg.flashback.ui.base.BaseActivity
 import tmg.utilities.extensions.loadFragment
+import java.lang.NullPointerException
 
 class RaceActivity : BaseActivity() {
 
     private lateinit var binding: ActivityRaceBinding
 
-    private lateinit var raceData: RaceData
+    private val crashController: CrashController by inject()
 
     companion object {
         private const val keyRaceData: String = "keyRaceData"
@@ -29,10 +32,11 @@ class RaceActivity : BaseActivity() {
         binding = ActivityRaceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        intent?.extras?.let {
-            raceData = it.getParcelable(keyRaceData)!!
+        val raceData: RaceData = intent?.extras?.getParcelable(keyRaceData) ?: run {
+            crashController.logException(NullPointerException("RaceActivity resumed with no race data, finishing activity"))
+            finish()
+            return
         }
-
         loadFragment(RaceFragment.instance(raceData), R.id.container)
     }
 }
