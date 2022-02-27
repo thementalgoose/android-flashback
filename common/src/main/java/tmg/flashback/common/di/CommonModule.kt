052d2c5f392ef705f9ca637bdf656775ce89199d
Jordan Fisher <thementalgoose@gmail.com>
2022-02-27 21:39:01 +0000

    Removes old settings VM for notifications, removes debug flag around results, adds experimental flag support for settings

--- common/src/main/java/tmg/flashback/common/di/CommonModule.kt
+++ common/src/main/java/tmg/flashback/common/di/CommonModule.kt
@@ -13,7 +13,6 @@ import tmg.flashback.common.ui.settings.appearance.SettingsAppearanceViewModel
 import tmg.flashback.common.ui.settings.appearance.animation.AnimationSpeedViewModel
 import tmg.flashback.common.ui.settings.appearance.nightmode.NightMoveViewModel
 import tmg.flashback.common.ui.settings.appearance.theme.ThemeViewModel
-import tmg.flashback.common.ui.settings.notifications.SettingsNotificationViewModel
 import tmg.flashback.common.ui.settings.support.SettingsSupportViewModel
 
 val commonModule = module {
@@ -30,7 +29,6 @@ val commonModule = module {
     viewModel { SettingsAppearanceViewModel(get(), get()) }
     viewModel { SettingsAboutViewModel() }
     viewModel { SettingsSupportViewModel(get(), get()) }
-    viewModel { SettingsNotificationViewModel(get()) }
     viewModel { NightMoveViewModel(get()) }
     viewModel { ThemeViewModel(get()) }
     viewModel { AnimationSpeedViewModel(get()) }
