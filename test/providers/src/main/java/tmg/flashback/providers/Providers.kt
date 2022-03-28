6162673b484e0fffe9546325265999e68b1bc7d6
Jordan Fisher <thementalgoose@gmail.com>
2022-03-28 23:25:15 +0100

    WIP sorting preview generation issue

--- test/providers/src/main/java/tmg/flashback/providers/Providers.kt
+++ test/providers/src/main/java/tmg/flashback/providers/Providers.kt
@@ -3,42 +3,42 @@ package tmg.flashback.providers
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import tmg.flashback.formula1.model.*
 
-//class DriverProvider: PreviewParameterProvider<Driver> {
-//    override val values: Sequence<Driver> = sequenceOf(Driver.model())
-//}
-//
-//class ConstructorProvider: PreviewParameterProvider<Constructor> {
-//    override val values: Sequence<Constructor> = sequenceOf(Constructor.model())
-//}
-//
-//class OverviewRaceProvider: PreviewParameterProvider<OverviewRace> {
-//    override val values: Sequence<OverviewRace> = sequenceOf(OverviewRace.model())
-//}
-//
-//class SeasonConstructorStandingsProvider: PreviewParameterProvider<SeasonConstructorStandings> {
-//    override val values: Sequence<SeasonConstructorStandings> = sequenceOf(SeasonConstructorStandings.model())
-//}
-//
-//class SeasonConstructorStandingSeasonProvider: PreviewParameterProvider<SeasonConstructorStandingSeason> {
-//    override val values: Sequence<SeasonConstructorStandingSeason> = sequenceOf(SeasonConstructorStandingSeason.model())
-//}
-//
-//class SeasonDriverStandingsProvider: PreviewParameterProvider<SeasonDriverStandings> {
-//    override val values: Sequence<SeasonDriverStandings> = sequenceOf(SeasonDriverStandings.model())
-//}
-//
-//class SeasonDriverStandingSeasonProvider: PreviewParameterProvider<SeasonDriverStandingSeason> {
-//    override val values: Sequence<SeasonDriverStandingSeason> = sequenceOf(SeasonDriverStandingSeason.model())
-//}
-//
-//class ScheduleProvider: PreviewParameterProvider<Schedule> {
-//    override val values: Sequence<Schedule> = sequenceOf(Schedule.model())
-//}
-//
-//class OverviewProvider: PreviewParameterProvider<Overview> {
-//    override val values: Sequence<Overview> = sequenceOf(Overview.model())
-//}
-//
-//class RaceProvider: PreviewParameterProvider<Race> {
-//    override val values: Sequence<Race> = sequenceOf(Race.model())
-//}
+class DriverProvider: PreviewParameterProvider<Driver> {
+    override val values: Sequence<Driver> = sequenceOf(Driver.model())
+}
+
+class ConstructorProvider: PreviewParameterProvider<Constructor> {
+    override val values: Sequence<Constructor> = sequenceOf(Constructor.model())
+}
+
+class OverviewRaceProvider: PreviewParameterProvider<OverviewRace> {
+    override val values: Sequence<OverviewRace> = sequenceOf(OverviewRace.model())
+}
+
+class SeasonConstructorStandingsProvider: PreviewParameterProvider<SeasonConstructorStandings> {
+    override val values: Sequence<SeasonConstructorStandings> = sequenceOf(SeasonConstructorStandings.model())
+}
+
+class SeasonConstructorStandingSeasonProvider: PreviewParameterProvider<SeasonConstructorStandingSeason> {
+    override val values: Sequence<SeasonConstructorStandingSeason> = sequenceOf(SeasonConstructorStandingSeason.model())
+}
+
+class SeasonDriverStandingsProvider: PreviewParameterProvider<SeasonDriverStandings> {
+    override val values: Sequence<SeasonDriverStandings> = sequenceOf(SeasonDriverStandings.model())
+}
+
+class SeasonDriverStandingSeasonProvider: PreviewParameterProvider<SeasonDriverStandingSeason> {
+    override val values: Sequence<SeasonDriverStandingSeason> = sequenceOf(SeasonDriverStandingSeason.model())
+}
+
+class ScheduleProvider: PreviewParameterProvider<Schedule> {
+    override val values: Sequence<Schedule> = sequenceOf(Schedule.model())
+}
+
+class OverviewProvider: PreviewParameterProvider<Overview> {
+    override val values: Sequence<Overview> = sequenceOf(Overview.model())
+}
+
+class RaceProvider: PreviewParameterProvider<Race> {
+    override val values: Sequence<Race> = sequenceOf(Race.model())
+}
