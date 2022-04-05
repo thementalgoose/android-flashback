473ef9a8228c4a677eca480e9c0d570520416241
Jordan Fisher <thementalgoose@gmail.com>
2022-04-06 00:12:28 +0100

    Adds navigation bar animation

--- presentation/ui/src/main/java/tmg/flashback/ui/components/navigation/NavigationBar.kt
+++ presentation/ui/src/main/java/tmg/flashback/ui/components/navigation/NavigationBar.kt
@@ -1,17 +1,31 @@
 package tmg.flashback.ui.components.navigation
 
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import tmg.flashback.style.AppTheme
 import tmg.flashback.style.AppThemePreview
+
+private val selectedPillWidth: Dp = 48.dp
+private val iconSize: Dp = 24.dp
 
 @Composable
 fun NavigationBar(
@@ -30,7 +44,10 @@ fun NavigationBar(
                     itemClicked(item)
                 },
                 icon = {
-                    Icon(painter = painterResource(id = item.icon), contentDescription = item.label?.let { stringResource(id = it) })
+                    SelectedIcon(
+                        isSelected = item.isSelected == true,
+                        icon = item.icon
+                    )
                 },
                 label = {
                     item.label?.let { label ->
@@ -42,6 +59,42 @@ fun NavigationBar(
                 alwaysShowLabel = true,
             )
         }
+    }
+}
+
+@Composable
+private fun SelectedIcon(
+    isSelected: Boolean,
+    @DrawableRes
+    icon: Int,
+    modifier: Modifier = Modifier
+) {
+    val selectedWidth = animateDpAsState(targetValue = when (isSelected) {
+        true -> selectedPillWidth
+        false -> iconSize
+    })
+    val backgroundColor = animateColorAsState(targetValue = when (isSelected) {
+        true -> AppTheme.colors.primary.copy(alpha = 0.2f)
+        false -> Color.Transparent
+    })
+
+    Box(modifier = modifier
+        .width(selectedPillWidth)
+        .height(iconSize)
+    ) {
+        Box(modifier = Modifier
+            .width(selectedWidth.value)
+            .height(iconSize)
+            .offset(x = (selectedPillWidth - selectedWidth.value) / 2)
+            .clip(RoundedCornerShape(iconSize / 2))
+            .background(backgroundColor.value))
+        Icon(
+            modifier = Modifier
+                .size(iconSize)
+                .align(Alignment.Center),
+            painter = painterResource(id = icon),
+            contentDescription = null,
+        )
     }
 }
 
--- stats/src/main/java/tmg/flashback/stats/ui/dashboard/drivers/DriverStandingsScreen.kt
+++ stats/src/main/java/tmg/flashback/stats/ui/dashboard/drivers/DriverStandingsScreen.kt
@@ -1,7 +1,155 @@
 package tmg.flashback.stats.ui.dashboard.drivers
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import tmg.flashback.formula1.extensions.pointsDisplay
+import tmg.flashback.formula1.model.SeasonDriverStandingSeason
+import tmg.flashback.formula1.model.SeasonDriverStandings
+import tmg.flashback.formula1.utils.getFlagResourceAlpha3
+import tmg.flashback.providers.SeasonDriverStandingSeasonProvider
+import tmg.flashback.providers.SeasonDriverStandingsProvider
+import tmg.flashback.style.AppTheme
+import tmg.flashback.style.text.TextBody2
+import tmg.flashback.style.text.TextTitle
+import tmg.flashback.stats.R
+import tmg.flashback.style.AppThemePreview
+import tmg.flashback.ui.components.progressbar.ProgressBar
+import tmg.flashback.ui.utils.isInPreview
+import kotlin.math.roundToInt
 
 @Composable
 fun DriverStandingsScreen() {
+
+}
+
+@Composable
+fun DriverStandings(
+    model: SeasonDriverStandingSeason,
+    itemClicked: (SeasonDriverStandingSeason) -> Unit,
+    maxPoints: Double,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .clickable(onClick = {
+                itemClicked(model)
+            }),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        TextTitle(
+            text = model.championshipPosition?.toString() ?: "-",
+            bold = true,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.width(42.dp)
+        )
+        Box(modifier = Modifier
+            .size(36.dp)
+            .clip(RoundedCornerShape(AppTheme.dimensions.radiusSmall))
+            .background(AppTheme.colors.backgroundSecondary)
+        ) {
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop,
+                model = model.driver.photoUrl ?: R.drawable.unknown_avatar,
+                contentDescription = null
+            )
+        }
+        Row(modifier = Modifier
+            .padding(
+                top = AppTheme.dimensions.paddingSmall,
+                start = AppTheme.dimensions.paddingSmall,
+                end = AppTheme.dimensions.paddingMedium,
+                bottom = AppTheme.dimensions.paddingSmall
+            )
+            .wrapContentHeight()
+        ) {
+            Column(modifier = Modifier.weight(2f)) {
+                TextTitle(
+                    text = model.driver.name,
+                    bold = true,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+                Row(modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(
+                        vertical = AppTheme.dimensions.paddingXXSmall,
+                    )
+                ) {
+                    val resourceId = when (isInPreview()) {
+                        true -> R.drawable.gb
+                        false -> LocalContext.current.getFlagResourceAlpha3(model.driver.nationalityISO)
+                    }
+                    Image(
+                        modifier = Modifier.size(16.dp),
+                        painter = painterResource(id = resourceId),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                    )
+                    Spacer(Modifier.width(4.dp))
+                    TextBody2(
+                        text = model.constructors.joinToString { it.constructor.name },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+            Spacer(Modifier.width(AppTheme.dimensions.paddingSmall))
+            val progress = (model.points / maxPoints).toFloat().coerceIn(0f, 1f)
+            ProgressBar(
+                modifier = Modifier
+                    .weight(2f)
+                    .fillMaxHeight(),
+                endProgress = progress,
+                barColor = model.constructors.lastOrNull()?.constructor?.colour ?: AppTheme.colors.primary,
+                label = {
+                    when (it) {
+                        0f -> "0"
+                        progress -> model.points.pointsDisplay()
+                        else -> (it * maxPoints).roundToInt().toString()
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewLight(
+    @PreviewParameter(SeasonDriverStandingSeasonProvider::class) driverStandings: SeasonDriverStandingSeason
+) {
+    AppThemePreview(isLight = true) {
+        DriverStandings(
+            model = driverStandings,
+            itemClicked = {},
+            maxPoints = 25.0)
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewDark(
+    @PreviewParameter(SeasonDriverStandingSeasonProvider::class) driverStandings: SeasonDriverStandingSeason
+) {
+    AppThemePreview(isLight = false) {
+        DriverStandings(
+            model = driverStandings,
+            itemClicked = {},
+            maxPoints = 25.0)
+    }
 }
