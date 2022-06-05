54de8798fb95a75f8bfbaa60bac24737d05139be
Jordan Fisher <thementalgoose@gmail.com>
2022-06-05 18:37:51 +0100

    Adds shared driver image section

--- feature/stats/src/main/java/tmg/flashback/stats/ui/search/SearchScreen.kt
+++ feature/stats/src/main/java/tmg/flashback/stats/ui/search/SearchScreen.kt
@@ -1,11 +1,13 @@
 package tmg.flashback.stats.ui.search
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Badge
 import androidx.compose.runtime.Composable
@@ -13,16 +15,20 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import org.koin.androidx.compose.viewModel
 import tmg.flashback.stats.R
+import tmg.flashback.stats.ui.shared.DriverImage
 import tmg.flashback.style.AppTheme
 import tmg.flashback.style.buttons.ButtonTertiary
 import tmg.flashback.style.input.InputPrimary
 import tmg.flashback.style.text.TextBody1
+import tmg.flashback.style.text.TextBody2
 import tmg.flashback.ui.components.header.Header
 
 @Composable
@@ -78,7 +84,11 @@ fun SearchScreen(
             }
             val text = remember { mutableStateOf(TextFieldValue("")) }
             InputPrimary(
-                modifier = Modifier.padding(horizontal = AppTheme.dimensions.paddingMedium),
+                modifier = Modifier.padding(
+                    start = AppTheme.dimensions.paddingMedium,
+                    end = AppTheme.dimensions.paddingMedium,
+                    bottom = AppTheme.dimensions.paddingSmall
+                ),
                 text = text,
                 onValueChange = {
                     text.value = it
@@ -86,6 +96,7 @@ fun SearchScreen(
                 },
                 placeholder = "INFO"
             )
+
         }
         items(list, key = { it.id }) {
             when (it) {
@@ -103,28 +114,61 @@ fun SearchScreen(
 
 @Composable
 private fun ResultCircuit(
-    model: SearchItem.Circuit
+    model: SearchItem.Circuit,
+    modifier: Modifier = Modifier
 ) {
+    Row { 
+        
+    }
     TextBody1("Circuit ${model.name}")
 }
 
 @Composable
 private fun ResultRace(
-    model: SearchItem.Race
+    model: SearchItem.Race,
+    modifier: Modifier = Modifier
 ) {
     TextBody1("Race ${model.raceName}")
 }
 
 @Composable
 private fun ResultDriver(
-    model: SearchItem.Driver
+    model: SearchItem.Driver,
+    modifier: Modifier = Modifier
 ) {
-    TextBody1("Driver ${model.name}")
+    Row(modifier = modifier.padding(
+        vertical = AppTheme.dimensions.paddingSmall,
+        horizontal = AppTheme.dimensions.paddingMedium
+    )) {
+        Box(Modifier
+            .size(42.dp)
+            .clip(RoundedCornerShape(AppTheme.dimensions.radiusSmall))
+            .background(AppTheme.colors.backgroundTertiary)
+        ) {
+            DriverImage(
+                size = 42.dp,
+                photoUrl = model.imageUrl
+            )
+        }
+        Spacer(Modifier.width(AppTheme.dimensions.paddingNSmall))
+        Column(modifier = Modifier.weight(1f)) {
+            TextBody1(
+                text = model.name,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(bottom = AppTheme.dimensions.paddingXSmall)
+            )
+            TextBody2(
+                text = model.nationality,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
 }
 
 @Composable
 private fun ResultConstructor(
-    model: SearchItem.Constructor
+    model: SearchItem.Constructor,
+    modifier: Modifier = Modifier
 ) {
     TextBody1("Constructor ${model.name}")
 }
