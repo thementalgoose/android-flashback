1ac82f1abf4eb6e5814f75dbd6a35969f76f280f
Jordan Fisher <thementalgoose@gmail.com>
2022-06-08 23:06:31 +0100

    Adds boilerplate for circuit activity

---
+++ feature/stats/src/main/java/tmg/flashback/stats/ui/circuits/CircuitScreen.kt
@@ -0,0 +1,16 @@
+package tmg.flashback.stats.ui.circuits
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CircuitScreenVM(
+    circuitId: String,
+    circuitName: String
+) {
+
+}
+
+@Composable
+fun CircuitScreen() {
+
+}
