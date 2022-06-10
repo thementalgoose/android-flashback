b2baa54d4e8be80febddee2e5020f87e82f2eed6
Jordan Fisher <thementalgoose@gmail.com>
2022-06-10 21:23:33 +0100

    Updates network circuit mapper tests

--- data/repo/src/test/java/tmg/flashback/statistics/repo/mappers/network/NetworkCircuitMapperTest.kt
+++ data/repo/src/test/java/tmg/flashback/statistics/repo/mappers/network/NetworkCircuitMapperTest.kt
@@ -1,54 +1,41 @@
 package tmg.flashback.statistics.repo.mappers.network
 
-import io.mockk.every
-import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import tmg.flashback.NetworkCircuitHistory
 import tmg.flashback.statistics.network.models.circuits.CircuitResult
 import tmg.flashback.statistics.network.models.circuits.model
-import tmg.flashback.statistics.room.models.circuit.*
+import tmg.flashback.statistics.room.models.circuit.CircuitRound
+import tmg.flashback.statistics.room.models.circuit.CircuitRoundResult
+import tmg.flashback.statistics.room.models.circuit.model
 
 internal class NetworkCircuitMapperTest {
-
-    private val mockNetworkCircuitDataMapper: NetworkCircuitDataMapper = mockk(relaxed = true)
 
     private lateinit var sut: NetworkCircuitMapper
 
     @BeforeEach
     internal fun setUp() {
-        sut = NetworkCircuitMapper(mockNetworkCircuitDataMapper)
-
-        every { mockNetworkCircuitDataMapper.mapCircuitData(any()) } returns Circuit.model()
+        sut = NetworkCircuitMapper()
     }
 
     @Test
-    fun `mapCircuit maps fields correctly`() {
-        val input = NetworkCircuitHistory.model()
-        val expected = CircuitHistory.model()
-
-        assertEquals(expected, sut.mapCircuit(input))
-    }
-
-    @Test
-    fun `mapCircuit returns null if input is null`() {
-        assertNull(sut.mapCircuit(null))
-    }
-
-    @Test
-    fun `mapCircuit with null results map has empty list of races`() {
-        val input = NetworkCircuitHistory.model(results = null)
-        assertEquals(emptyList<CircuitRoundWithResults>(), sut.mapCircuit(input)!!.races)
-    }
-
-    @Test
-    fun `mapCircuitRounds maps fields correctly`() {
-        val inputCircuitId: String = "circuitId"
-        val inputCircuitRound: CircuitResult = CircuitResult.model()
+    fun `map circuit rounds maps fields correctly`() {
+        val input = CircuitResult.model()
         val expected = CircuitRound.model()
 
-        assertEquals(expected, sut.mapCircuitRounds(inputCircuitId, inputCircuitRound))
+        assertEquals(expected, sut.mapCircuitRounds(expected.circuitId, input))
+    }
+
+    @Test
+    fun `map circuit previews maps fields correctly`() {
+        val input = CircuitResult.model()
+        val expected = listOf(CircuitRoundResult.model())
+
+        assertEquals(expected, sut.mapCircuitPreviews(input))
+    }
+
+    @Test
+    fun `map circuit previews returns empty list when mapping null`() {
+        assertEquals(emptyList<CircuitRoundResult>(), sut.mapCircuitPreviews(CircuitResult.model(preview = null)))
     }
 }
