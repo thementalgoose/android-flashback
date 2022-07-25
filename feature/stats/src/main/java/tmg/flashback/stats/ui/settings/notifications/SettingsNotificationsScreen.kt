68c6bd7c634426304eecf89123c960d28bab548f
Jordan Fisher <thementalgoose@gmail.com>
2022-07-25 16:08:44 +0100

    Settings Notification ScreenView

--- feature/stats/src/main/java/tmg/flashback/stats/ui/settings/notifications/SettingsNotificationsScreen.kt
+++ feature/stats/src/main/java/tmg/flashback/stats/ui/settings/notifications/SettingsNotificationsScreen.kt
@@ -10,6 +10,7 @@ import tmg.flashback.stats.R
 import tmg.flashback.stats.repository.NotificationRepository
 import tmg.flashback.stats.repository.models.NotificationReminder
 import tmg.flashback.ui.bottomsheet.BottomSheetItem
+import tmg.flashback.ui.components.analytics.ScreenView
 import tmg.flashback.ui.components.settings.SettingsScreen
 import tmg.utilities.lifecycle.Event
 import tmg.utilities.models.Selected
@@ -20,6 +21,8 @@ fun SettingsNotificationScreenVM(
     actionUpClicked: () -> Unit
 ) {
     val viewModel by viewModel<SettingsNotificationViewModel>()
+    
+    ScreenView(screenName = "Settings Notifications")
 
     SettingsScreen(
         title = stringResource(id = R.string.settings_notifications_title),
