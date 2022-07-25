40c43d20f351f1e0e610853b136b8ec9c110f8e3
Jordan Fisher <thementalgoose@gmail.com>
2022-07-25 16:28:19 +0100

    Settings About ScreenView

--- feature/settings/src/main/java/tmg/flashback/settings/ui/settings/about/SettingsAboutScreen.kt
+++ feature/settings/src/main/java/tmg/flashback/settings/ui/settings/about/SettingsAboutScreen.kt
@@ -4,12 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import org.koin.androidx.compose.viewModel
 import tmg.flashback.settings.R
+import tmg.flashback.ui.components.analytics.ScreenView
 import tmg.flashback.ui.components.settings.SettingsScreen
 
 @Composable
 fun SettingsAboutScreenVM(
     actionUpClicked: () -> Unit
 ) {
+    ScreenView(screenName = "Settings About")
+    
     val viewModel by viewModel<SettingsAboutViewModel>()
 
     SettingsScreen(
