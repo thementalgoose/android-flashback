be80afb7bd8713d1ba4caad6b40fe83ffc970ea7
Jordan Fisher <thementalgoose@gmail.com>
2022-07-25 16:30:33 +0100

    Settings Web Browser ScreenView

--- core/webbrowser/src/main/java/tmg/flashback/web/ui/settings/SettingsWebBrowserScreen.kt
+++ core/webbrowser/src/main/java/tmg/flashback/web/ui/settings/SettingsWebBrowserScreen.kt
@@ -3,6 +3,7 @@ package tmg.flashback.web.ui.settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import org.koin.androidx.compose.viewModel
+import tmg.flashback.ui.components.analytics.ScreenView
 import tmg.flashback.ui.components.settings.SettingsScreen
 import tmg.flashback.web.R
 
@@ -10,6 +11,8 @@ import tmg.flashback.web.R
 fun SettingsWebBrowserScreenVM(
     actionUpClicked: () -> Unit
 ) {
+    ScreenView(screenName = "Settings Web Browser")
+
     val viewModel by viewModel<SettingsWebBrowserViewModel>()
 
     SettingsScreen(
