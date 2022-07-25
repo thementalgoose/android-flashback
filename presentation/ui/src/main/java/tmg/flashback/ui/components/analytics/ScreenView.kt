50b0638b28350f75cbbf7f03f6cf6b5f8183be14
Jordan Fisher <thementalgoose@gmail.com>
2022-07-25 16:04:35 +0100

    Adds initial ScreenView analytic composable

---
+++ presentation/ui/src/main/java/tmg/flashback/ui/components/analytics/ScreenView.kt
@@ -0,0 +1,22 @@
+package tmg.flashback.ui.components.analytics
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import org.koin.androidx.compose.inject
+import tmg.flashback.analytics.manager.AnalyticsManager
+
+@Composable
+fun ScreenView(
+    screenName: String,
+    args: Map<String, Any> = mapOf()
+) {
+//    val analyticsManager: AnalyticsManager by inject()
+    DisposableEffect(key1 = Unit, effect = {
+        println("SCREEN VIEW $screenName")
+//        analyticsManager.viewScreen(screenName, args)
+        this.onDispose {
+            println("SCREEN Navigate up $screenName")
+//            analyticsManager.logEvent("")
+        }
+    })
+}
