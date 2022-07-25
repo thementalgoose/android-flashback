1d35e943ba235bb58026933e765d8d472b4dc59b
Jordan Fisher <thementalgoose@gmail.com>
2022-07-25 16:23:30 +0100

    Settings Advert ScreenView

--- ads/src/main/java/tmg/flashback/ads/ui/settings/adverts/SettingsAdvertScreen.kt
+++ ads/src/main/java/tmg/flashback/ads/ui/settings/adverts/SettingsAdvertScreen.kt
@@ -4,12 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import org.koin.androidx.compose.viewModel
 import tmg.flashback.ads.R
+import tmg.flashback.ui.components.analytics.ScreenView
 import tmg.flashback.ui.components.settings.SettingsScreen
 
 @Composable
 fun SettingsAdvertScreenVM(
     actionUpClicked: () -> Unit
 ) {
+    ScreenView(screenName = "Settings Adverts")
+    
     val viewModel by viewModel<SettingsAdvertViewModel>()
 
     SettingsScreen(
