ed4df17cefd719dbfb698c8f570894f5a431bf73
Jordan Fisher <thementalgoose@gmail.com>
2022-07-25 16:11:43 +0100

    Onboarding Notification ScreenView

--- feature/stats/src/main/java/tmg/flashback/stats/ui/feature/notificationonboarding/NotificationOnboardingScreen.kt
+++ feature/stats/src/main/java/tmg/flashback/stats/ui/feature/notificationonboarding/NotificationOnboardingScreen.kt
@@ -20,12 +20,15 @@ import tmg.flashback.style.AppTheme
 import tmg.flashback.style.AppThemePreview
 import tmg.flashback.style.annotations.PreviewTheme
 import tmg.flashback.style.text.TextBody1
+import tmg.flashback.ui.components.analytics.ScreenView
 import tmg.flashback.ui.components.layouts.BottomSheet
 import tmg.utilities.models.Selected
 
 @Composable
 fun NotificationOnboardingScreenVM() {
     val viewModel by viewModel<NotificationOnboardingViewModel>()
+    
+    ScreenView(screenName = "Onboarding Notifications")
 
     val list = viewModel.outputs.notificationPreferences.observeAsState(emptyList())
 
