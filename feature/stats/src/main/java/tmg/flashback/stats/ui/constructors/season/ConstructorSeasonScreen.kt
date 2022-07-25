ffa89cdc63fe470d4004c0d85e00eb4b217b36c9
Jordan Fisher <thementalgoose@gmail.com>
2022-07-25 16:16:50 +0100

    Circuit ScreenView

--- feature/stats/src/main/java/tmg/flashback/stats/ui/constructors/season/ConstructorSeasonScreen.kt
+++ feature/stats/src/main/java/tmg/flashback/stats/ui/constructors/season/ConstructorSeasonScreen.kt
@@ -1,6 +1,9 @@
 package tmg.flashback.stats.ui.constructors.season
 
 import androidx.compose.runtime.Composable
+import tmg.flashback.stats.analytics.AnalyticsConstants.analyticsConstructorId
+import tmg.flashback.stats.analytics.AnalyticsConstants.analyticsSeason
+import tmg.flashback.ui.components.analytics.ScreenView
 
 @Composable
 fun ConstructorSeasonScreenVM(
@@ -8,7 +11,10 @@ fun ConstructorSeasonScreenVM(
     constructorName: String,
     season: Int
 ) {
-
+    ScreenView(screenName = "Constructor Season", args = mapOf(
+        analyticsConstructorId to constructorId,
+        analyticsSeason to season.toString()
+    ))
 }
 
 @Composable
