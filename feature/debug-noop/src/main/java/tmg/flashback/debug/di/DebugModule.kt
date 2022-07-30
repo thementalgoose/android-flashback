e4ce6958a0e63f26e2303632405825b9e61984cf
Jordan Fisher <thementalgoose@gmail.com>
2022-07-30 13:09:26 +0100

    Migrates di for :debug

--- feature/debug-noop/src/main/java/tmg/flashback/debug/di/DebugModule.kt
+++ feature/debug-noop/src/main/java/tmg/flashback/debug/di/DebugModule.kt
@@ -1,11 +1,17 @@
 package tmg.flashback.debug.di
 
-import org.koin.dsl.module
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import tmg.flashback.debug.DebugNavigationComponent
 import tmg.flashback.debug.manager.DebugBaseUrlOverrideManager
 import tmg.flashback.statistics.network.manager.BaseUrlLocalOverrideManager
 
-val debugModule = module {
-    single<BaseUrlLocalOverrideManager> { DebugBaseUrlOverrideManager() }
-    single { DebugNavigationComponent() }
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DebugModule {
+
+    @Binds
+    abstract fun bindsBaseUrlLocalOverrideManager(impl: DebugBaseUrlOverrideManager): BaseUrlLocalOverrideManager
 }
