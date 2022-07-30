fd4bd242cc75914ad555f8ff3937c4df42276d9c
Jordan Fisher <thementalgoose@gmail.com>
2022-07-30 12:49:46 +0100

    Migrates di for :core:appshortcuts

--- core/appshortcuts/src/main/java/tmg/flashback/appshortcuts/di/AppShortcutModule.kt
+++
@@ -1,8 +0,0 @@
-package tmg.flashback.appshortcuts.di
-
-import org.koin.dsl.module
-import tmg.flashback.appshortcuts.manager.AppShortcutManager
-
-val appShortcutModule = module {
-    single { AppShortcutManager(get(), get()) }
-}
