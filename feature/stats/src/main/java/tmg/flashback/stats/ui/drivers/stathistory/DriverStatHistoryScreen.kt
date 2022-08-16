5639a967b03a75c55e0f162f3de35ce050d1eb1b
Jordan Fisher <thementalgoose@gmail.com>
2022-08-16 09:17:12 +0100

    Adds label usage for header of driver specific stat

--- feature/stats/src/main/java/tmg/flashback/stats/ui/drivers/stathistory/DriverStatHistoryScreen.kt
+++ feature/stats/src/main/java/tmg/flashback/stats/ui/drivers/stathistory/DriverStatHistoryScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.lifecycle.viewmodel.compose.viewModel
 import tmg.flashback.formula1.model.Race
@@ -56,7 +57,7 @@ private fun DriverStatHistoryScreen(
         content = {
             item(key = "header") {
                 Header(
-                    text = "$driverName\n$driverStatHistoryType",
+                    text = "$driverName\n${stringResource(id = driverStatHistoryType.label)}",
                     icon = painterResource(id = R.drawable.ic_web_close),
                     iconContentDescription = null,
                     actionUpClicked = actionUpClicked
