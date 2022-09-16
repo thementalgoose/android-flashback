b3de2346cdc824940daf773a7e43edd10034a197
Jordan Fisher <thementalgoose@gmail.com>
2022-09-16 16:33:58 +0100

    Fixes unit tests

--- app/src/test/java/tmg/flashback/ui/settings/layout/SettingsLayoutViewModelTest.kt
+++ app/src/test/java/tmg/flashback/ui/settings/layout/SettingsLayoutViewModelTest.kt
@@ -23,46 +23,6 @@ internal class SettingsLayoutViewModelTest: BaseTest() {
     }
 
     @Test
-    fun `menu all expanded is true when pref is true`() {
-        every { mockHomeRepository.showListAll } returns true
-
-        initUnderTest()
-        underTest.outputs.menuAllExpandedEnabled.test {
-            assertValue(true)
-        }
-    }
-
-    @Test
-    fun `menu all expanded is false when pref is false`() {
-        every { mockHomeRepository.showListAll } returns false
-
-        initUnderTest()
-        underTest.outputs.menuAllExpandedEnabled.test {
-            assertValue(false)
-        }
-    }
-
-    @Test
-    fun `menu favourite expanded is true when pref is true`() {
-        every { mockHomeRepository.showListFavourited } returns true
-
-        initUnderTest()
-        underTest.outputs.menuFavouriteExpandedEnabled.test {
-            assertValue(true)
-        }
-    }
-
-    @Test
-    fun `menu favourite expanded is false when pref is false`() {
-        every { mockHomeRepository.showListFavourited } returns false
-
-        initUnderTest()
-        underTest.outputs.menuFavouriteExpandedEnabled.test {
-            assertValue(false)
-        }
-    }
-
-    @Test
     fun `provided by at top is true when pref is true`() {
         every { mockHomeRepository.dataProvidedByAtTop } returns true
 
@@ -80,34 +40,6 @@ internal class SettingsLayoutViewModelTest: BaseTest() {
         underTest.outputs.providedByAtTopEnabled.test {
             assertValue(false)
         }
-    }
-
-    @Test
-    fun `click expanded all updates pref and updates value`() {
-        every { mockHomeRepository.showListAll } returns false
-
-        initUnderTest()
-        val observer = underTest.outputs.menuAllExpandedEnabled.testObserve()
-        underTest.inputs.prefClicked(Settings.Layout.menuAllExpanded(true))
-
-        verify {
-            mockHomeRepository.showListAll = true
-        }
-        observer.assertEmittedCount(2)
-    }
-
-    @Test
-    fun `click expanded favourite updates pref and updates value`() {
-        every { mockHomeRepository.showListFavourited } returns false
-
-        initUnderTest()
-        val observer = underTest.outputs.menuFavouriteExpandedEnabled.testObserve()
-        underTest.inputs.prefClicked(Settings.Layout.menuFavouriteExpanded(true))
-
-        verify {
-            mockHomeRepository.showListFavourited = true
-        }
-        observer.assertEmittedCount(2)
     }
 
     @Test
