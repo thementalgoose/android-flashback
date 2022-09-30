49d09fecf85ee9200800083e0a03ea4bd983f8c8
Jordan Fisher <thementalgoose@gmail.com>
2022-09-30 21:54:29 +0100

    Cleans up dependency list in AboutThisApp

--- app/src/main/java/tmg/flashback/constants/AboutThisAppConfig.kt
+++ app/src/main/java/tmg/flashback/constants/AboutThisAppConfig.kt
@@ -22,10 +22,10 @@ object AboutThisAppConfig {
     ): AboutThisAppConfiguration {
         val dependenciesList = getDependencies()
         val list = dependenciesList +
-                getRssSourcesDependencies(context, dependenciesList.size, rssSources)
+                getRssSourcesDependencies(context, rssSources)
         val assets = listOf(
                 AboutThisAppDependency(
-                        order = list.size + 1,
+                        order = 0,
                         dependencyName = "Lottie: LottieFiles",
                         author = "LottieFiles",
                         imageUrl = "https://assets4.lottiefiles.com/avatars/300_8243-998405194.jpg",
@@ -33,7 +33,7 @@ object AboutThisAppConfig {
                         url = "https://lottiefiles.com/LottieFiles"
                 ),
                 AboutThisAppDependency(
-                        order = list.size + 1,
+                        order = 0,
                         dependencyName = "Lottie: Baback Jafari",
                         author = "Baback Jafari",
                         imageUrl = "https://assets4.lottiefiles.com/avatars/300_8243-998405194.jpg",
@@ -41,7 +41,7 @@ object AboutThisAppConfig {
                         url = "https://lottiefiles.com/LottieFiles"
                 ),
                 AboutThisAppDependency(
-                        order = list.size + 1,
+                        order = 0,
                         dependencyName = "FlatIcon: Freepik",
                         author = "Freepik",
                         imageUrl = "https://img-authors.flaticon.com/freepik.jpg",
@@ -63,7 +63,9 @@ object AboutThisAppConfig {
                 appPackageName = "tmg.flashback",
                 email = "thementalgoose@gmail.com",
                 play = "https://play.google.com/store/apps/details?id=tmg.flashback",
-                dependencies = list + assets
+                dependencies = (list + assets).mapIndexed { index, item ->
+                        item.copy(order = index)
+                }
         )
     }
 
@@ -78,7 +80,7 @@ object AboutThisAppConfig {
                         imageUrl = "https://pbs.twimg.com/profile_images/204468195/logo_400x400.png"
                 ),
                 AboutThisAppDependency(
-                        order = 1,
+                        order = 0,
                         dependencyName = "Jetpack",
                         author = "Google",
                         url = "https://developer.android.com/jetpack",
@@ -86,7 +88,7 @@ object AboutThisAppConfig {
                         imageUrl = "https://avatars.githubusercontent.com/u/6955922?s=200&v=4"
                 ),
                 AboutThisAppDependency(
-                        order = 2,
+                        order = 0,
                         dependencyName = "Firebase",
                         author = "Google",
                         url = "https://firebase.google.com/",
@@ -94,7 +96,23 @@ object AboutThisAppConfig {
                         imageUrl = "https://avatars2.githubusercontent.com/u/1335026"
                 ),
                 AboutThisAppDependency(
-                        order = 3,
+                        order = 0,
+                        dependencyName = "Retrofit",
+                        author = "Square",
+                        url = "https://square.github.io/retrofit/",
+                        backgroundColor = Color.TRANSPARENT,
+                        imageUrl = "https://avatars.githubusercontent.com/u/82592"
+                ),
+                AboutThisAppDependency(
+                        order = 0,
+                        dependencyName = "OkHttp",
+                        author = "Square",
+                        url = "https://square.github.io/okhttp/",
+                        backgroundColor = Color.TRANSPARENT,
+                        imageUrl = "https://avatars.githubusercontent.com/u/82592"
+                ),
+                AboutThisAppDependency(
+                        order = 0,
                         dependencyName = "Glide",
                         author = "Bump Technologies",
                         url = "https://github.com/bumptech/glide",
@@ -102,15 +120,15 @@ object AboutThisAppConfig {
                         imageUrl = "https://lh3.googleusercontent.com/OOjYcooPxIC4PHWxKGg5tfVm5qbJB4m2IMvhmXCOMl9Ps4T6dvmcA66UscrkML0lU6WR0IfswAL9QNpEL63mpLvrtDMiLnOMYCT8rhkC-eIXjhDNk6wGlx-nMJeZzyrvairQOD48KnxhY9vc-tahh7rgKoJeR1mwfoJIVfBNRwlNTSrLkrDZFAU15fvGofmKCrrvlUgUka6tpD80A1-Dm3KRE9knS0m1UHssQ6-KOFdGSndZ70ayGV5pY-n-zDsMYAzDNQMwvb2AhUddiO6VOViXztUqiYuVX5eqCnL7z-bndTcDAqfyohvw8txH5bvc1VR0XcQPjGzJ6EVkdZso2T4b5NoFufzlIP3DPjoFE37VKEGmnI-QMhz9m_IwuJ2U0WXBP9Q4pJkVPqwbIZzm-g338ZETis17D3r52v4hDsq5mN7vzV5KcRHs5l1uivdS5Wj5SQ0t96xmndOEOUISyIxGWeeDGIVSImnK6GuLEfrO4Vsi9gc4Qi8KU5aDBZ0rsbTM-hgNObqBTs-AebwR9gspWCqW7Cigfnezbf1bHAyvPjoLaJ_2IxjoF9KZxjPieYRuXMoDpdhvT5_0cfEsUQF8HjR1qBPku_asce3UtQGvIhMikw=s0"
                 ),
                 AboutThisAppDependency(
-                        order = 4,
-                        dependencyName = "Koin",
-                        author = "Koin",
-                        url = "https://github.com/InsertKoinIO/koin",
-                        backgroundColor = Color.TRANSPARENT,
-                        imageUrl = "https://avatars1.githubusercontent.com/u/38280958"
+                        order = 0,
+                        dependencyName = "Coil",
+                        author = "Coil",
+                        url = "https://github.com/coil-kt/coil",
+                        backgroundColor = Color.WHITE,
+                        imageUrl = "https://avatars.githubusercontent.com/u/52722434"
                 ),
                 AboutThisAppDependency(
-                        order = 5,
+                        order = 0,
                         dependencyName = "FlagKit",
                         author = "WANG Jie",
                         url = "https://github.com/wangjiejacques/flagkit",
@@ -118,7 +136,7 @@ object AboutThisAppConfig {
                         imageUrl = "https://avatars3.githubusercontent.com/u/2981971"
                 ),
                 AboutThisAppDependency(
-                        order = 6,
+                        order = 0,
                         dependencyName = "Shakey",
                         author = "LinkedIn",
                         url = "https://github.com/linkedin/shaky-android",
@@ -126,7 +144,15 @@ object AboutThisAppConfig {
                         imageUrl = "https://avatars.githubusercontent.com/u/357098?s=200"
                 ),
                 AboutThisAppDependency(
-                        order = 7,
+                        order = 0,
+                        dependencyName = "Accompanist",
+                        author = "Google",
+                        url = "https://google.github.io/accompanist/",
+                        backgroundColor = Color.WHITE,
+                        imageUrl = "https://avatars.githubusercontent.com/u/1342004?s=200&v=4"
+                ),
+                AboutThisAppDependency(
+                        order = 0,
                         dependencyName = "ThreeTen",
                         author = "Jake Wharton",
                         url = "https://github.com/JakeWharton/ThreeTenABP",
@@ -134,15 +160,7 @@ object AboutThisAppConfig {
                         imageUrl = "https://avatars0.githubusercontent.com/u/66577"
                 ),
                 AboutThisAppDependency(
-                        order = 8,
-                        dependencyName = "OverlappingPanels",
-                        author = "Discord",
-                        url = "https://github.com/discord/OverlappingPanels",
-                        backgroundColor = Color.TRANSPARENT,
-                        imageUrl = "https://avatars3.githubusercontent.com/u/1965106"
-                ),
-                AboutThisAppDependency(
-                        order = 9,
+                        order = 0,
                         dependencyName = "SkeletonLayout",
                         author = "Faltenreich",
                         url = "https://github.com/Faltenreich/SkeletonLayout",
@@ -150,15 +168,7 @@ object AboutThisAppConfig {
                         imageUrl = "https://avatars3.githubusercontent.com/u/7239950"
                 ),
                 AboutThisAppDependency(
-                        order = 10,
-                        dependencyName = "Shimmer",
-                        author = "Facebook",
-                        url = "http://facebook.github.io/shimmer-android/",
-                        backgroundColor = Color.TRANSPARENT,
-                        imageUrl = "https://avatars.githubusercontent.com/u/69631"
-                ),
-                AboutThisAppDependency(
-                        order = 11,
+                        order = 0,
                         dependencyName = "Lottie",
                         author = "AirBnB",
                         url = "https://github.com/airbnb/lottie-android",
@@ -166,28 +176,20 @@ object AboutThisAppConfig {
                         imageUrl = "https://avatars2.githubusercontent.com/u/698437"
                 ),
                 AboutThisAppDependency(
-                        order = 12,
+                        order = 0,
                         dependencyName = "Labelled Progress Bar",
                         author = "Jordan Fisher",
                         url = "https://github.com/thementalgoose/android-labelled-progress-bar",
                         backgroundColor = Color.TRANSPARENT,
                         imageUrl = "https://avatars1.githubusercontent.com/u/5982159"
-                ),
-                AboutThisAppDependency(
-                        order = 13,
-                        dependencyName = "IndicatorFastScroll",
-                        author = "reddit",
-                        url = "https://github.com/reddit/IndicatorFastScroll",
-                        backgroundColor = Color.WHITE,
-                        imageUrl = "https://avatars.githubusercontent.com/u/14248?s=200&v=4"
                 )
         )
     }
 
-    private fun getRssSourcesDependencies(context: Context, startIndex: Int, sources: List<SupportedArticleSource>): List<AboutThisAppDependency> {
+    private fun getRssSourcesDependencies(context: Context, sources: List<SupportedArticleSource>): List<AboutThisAppDependency> {
         return sources.mapIndexed { index, supportedSource ->
             AboutThisAppDependency(
-                    order = startIndex + index,
+                    order = 0,
                     dependencyName = context.getString(R.string.about_this_app_dependency_rss, supportedSource.title),
                     author = supportedSource.title,
                     url = supportedSource.contactLink,
