6e12078be486dc7af34f219ad49c16fb8f4a93e7
Jordan Fisher <thementalgoose@gmail.com>
2022-11-06 00:07:13 +0000

    WIP Dashboard tablet UI

--- app/src/main/java/tmg/flashback/ui/dashboard/DashboardScreen.kt
+++ app/src/main/java/tmg/flashback/ui/dashboard/DashboardScreen.kt
@@ -7,29 +7,37 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
 import tmg.flashback.stats.ui.dashboard.schedule.ScheduleScreenVM
 import tmg.flashback.stats.ui.dashboard.constructors.ConstructorStandingsScreenVM
 import tmg.flashback.stats.ui.dashboard.drivers.DriverStandingsScreenVM
-import tmg.flashback.stats.usecases.DefaultSeasonUseCase
 import tmg.flashback.style.AppTheme
-import tmg.flashback.style.utils.WindowSize
 import tmg.flashback.ui.components.layouts.OverlappingPanels
 import tmg.flashback.ui.components.layouts.OverlappingPanelsValue
 import tmg.flashback.ui.components.layouts.rememberOverlappingPanelsState
 import tmg.flashback.ui.components.navigation.NavigationBar
+import tmg.flashback.ui.components.navigation.NavigationColumn
 import tmg.flashback.ui.components.navigation.NavigationItem
+import tmg.flashback.ui.components.navigation.NavigationTimelineItem
+import tmg.flashback.ui.components.navigation.PipeType
 import tmg.flashback.ui.components.navigation.appBarHeight
 import tmg.flashback.ui.dashboard.menu.MenuScreenVM
+import tmg.flashback.ui.dashboard.menu.MenuSeasonItem
+import tmg.flashback.ui.dashboard.menu.MenuViewModel
 import tmg.utilities.extensions.toEnum
 
 data class DashboardScreenState(
@@ -45,7 +53,8 @@ fun DashboardScreenVM() {
     DashboardScreen(
         tabState = viewModel.outputs.currentTab.observeAsState(viewModel.outputs.initialTab).value,
         clickTab = viewModel.inputs::clickTab,
-        clickSeason = viewModel.inputs::clickSeason)
+        clickSeason = viewModel.inputs::clickSeason
+    )
 }
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -150,6 +159,65 @@ fun DashboardScreen(
     )
 }
 
+@Composable
+fun DashboardScreenTabletVM() {
+
+    val dashboardViewModel = hiltViewModel<DashboardViewModel>()
+    val menuViewModel = hiltViewModel<MenuViewModel>()
+
+    DashboardScreenTablet(
+        tabState = dashboardViewModel.outputs.currentTab.observeAsState(dashboardViewModel.outputs.initialTab).value,
+        clickTab = dashboardViewModel.inputs::clickTab,
+        clickSeason = {
+            dashboardViewModel.inputs.clickSeason(it)
+            menuViewModel.inputs.clickSeason(it)
+        },
+        seasonList = menuViewModel.outputs.season.observeAsState(emptyList()).value
+    )
+}
+
+@Composable
+fun DashboardScreenTablet(
+    tabState: DashboardScreenState,
+    seasonList: List<MenuSeasonItem>,
+    clickTab: (DashboardNavItem) -> Unit,
+    clickSeason: (Int) -> Unit,
+) {
+    Row(Modifier.fillMaxSize()) {
+        NavigationColumn(
+            list = tabState.tab.toNavigationItems(),
+            menuClicked = {  },
+            itemClicked = {
+                val tab = it.id.toEnum() ?: DashboardNavItem.CALENDAR
+                clickTab(tab)
+            },
+            timelineList = seasonList.map {
+                NavigationTimelineItem(
+                    id = it.season.toString(),
+                    pipeType = when {
+                        it.isFirst -> PipeType.START
+                        it.isLast -> PipeType.END
+                        else -> PipeType.START_END
+                    },
+                    label = it.season.toString(),
+                    color = it.colour,
+                    isSelected = it.isSelected
+                )
+            },
+            timelineItemClicked = {
+                clickSeason(it.label.toInt())
+            }
+        )
+
+        Box(
+            Modifier
+                .width(400.dp)
+                .fillMaxHeight()
+                .background(AppTheme.colors.accent)
+        )
+    }
+}
+
 private fun DashboardNavItem.toNavigationItems(): List<NavigationItem> {
     return DashboardNavItem.values()
         .map {
@@ -161,3 +229,4 @@ private fun DashboardNavItem.toNavigationItems(): List<NavigationItem> {
             )
         }
 }
+
