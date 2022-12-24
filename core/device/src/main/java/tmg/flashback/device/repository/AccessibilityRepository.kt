fb1fe5653a88f9ecf3f931ecdc96c2131c53a33d
Jordan Fisher <thementalgoose@gmail.com>
2022-12-24 11:32:24 +0000

    Adds accessibility repository to :core:device

---
+++ core/device/src/main/java/tmg/flashback/device/repository/AccessibilityRepository.kt
@@ -0,0 +1,21 @@
+package tmg.flashback.device.repository
+
+import android.content.Context
+import android.provider.Settings
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class AccessibilityRepository @Inject constructor(
+    @ApplicationContext
+    private val applicationContext: Context
+) {
+    val isAnimationsEnabled: Boolean
+        get() = animatorValue == 0f
+
+    private val animatorValue: Float
+        get() = Settings.Global.getFloat(
+            applicationContext.contentResolver,
+            Settings.Global.ANIMATOR_DURATION_SCALE,
+            0f
+        )
+}
