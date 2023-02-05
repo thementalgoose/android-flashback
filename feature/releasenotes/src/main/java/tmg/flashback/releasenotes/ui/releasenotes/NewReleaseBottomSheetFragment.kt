d76cf1660b13f4eee7a7700bb8e471b166bbfca5
Jordan Fisher <thementalgoose@gmail.com>
2023-02-05 13:55:23 +0000

    Migrates release notes bottom sheet to use compose

---
+++ feature/releasenotes/src/main/java/tmg/flashback/releasenotes/ui/releasenotes/NewReleaseBottomSheetFragment.kt
@@ -0,0 +1,22 @@
+package tmg.flashback.releasenotes.ui.releasenotes
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import dagger.hilt.android.AndroidEntryPoint
+import tmg.flashback.releasenotes.repository.ReleaseNotesRepository
+import tmg.flashback.releasenotes.usecases.NewReleaseNotesUseCase
+import tmg.flashback.ui.base.BaseBottomSheetComposeFragment
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class NewReleaseBottomSheetFragment: BaseBottomSheetComposeFragment() {
+
+    @Inject
+    lateinit var releaseNotesRepository: ReleaseNotesRepository
+    @Inject
+    lateinit var newReleaseNotesUseCase: NewReleaseNotesUseCase
+
+    override val content: @Composable () -> Unit = {
+        NewReleaseScreen(releaseNotes = newReleaseNotesUseCase.getNotes())
+    }
+}
