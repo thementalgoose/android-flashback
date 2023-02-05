96dccec9a10ea4fe02272b57f30b3d4410f221f8
Jordan Fisher <thementalgoose@gmail.com>
2023-02-05 14:17:03 +0000

    Updates privacy policy to be its own module

--- feature/settings/src/main/java/tmg/flashback/settings/SettingsNavigationComponent.kt
+++ feature/settings/src/main/java/tmg/flashback/settings/SettingsNavigationComponent.kt
@@ -1,27 +1,12 @@
 package tmg.flashback.settings
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
-import tmg.flashback.releasenotes.ReleaseNotes
-import tmg.flashback.releasenotes.ui.releasenotes.ReleaseScreenVM
-import tmg.flashback.settings.ui.privacypolicy.PrivacyPolicyScreenVM
 import tmg.flashback.settings.ui.settings.appearance.nightmode.NightModeBottomSheetFragment
 import tmg.flashback.settings.ui.settings.appearance.theme.ThemeBottomSheetFragment
 import tmg.flashback.ui.navigation.ActivityProvider
-import tmg.flashback.ui.navigation.NavigationDestination
-import tmg.flashback.ui.navigation.Navigator
-import tmg.flashback.ui.navigation.Screen
 import javax.inject.Inject
 
-val Screen.Settings.PrivacyPolicy: NavigationDestination
-    get() = object : NavigationDestination {
-        override val route: String = "privacy_policy"
-    }
-
 class SettingsNavigationComponent @Inject constructor(
-    private val navigator: Navigator,
     private val activityProvider: ActivityProvider
 ) {
     fun nightModeDialog() = activityProvider.launch {
@@ -32,9 +17,5 @@ class SettingsNavigationComponent @Inject constructor(
     fun themeDialog() = activityProvider.launch {
         val activity = it as? AppCompatActivity ?: return@launch
         ThemeBottomSheetFragment().show(activity.supportFragmentManager, "THEME")
-    }
-
-    fun privacyPolicy() = activityProvider.launch {
-        navigator.navigate(Screen.Settings.PrivacyPolicy)
     }
 }
