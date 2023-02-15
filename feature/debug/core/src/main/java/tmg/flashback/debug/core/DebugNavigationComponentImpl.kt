737e95a8915192fe7a0c81dc276600cd9c90d6f9
Jordan Fisher <thementalgoose@gmail.com>
2023-02-15 18:06:31 +0000

    Reverted dev only settings

--- feature/debug/core/src/main/java/tmg/flashback/debug/core/DebugNavigationComponentImpl.kt
+++ feature/debug/core/src/main/java/tmg/flashback/debug/core/DebugNavigationComponentImpl.kt
@@ -29,12 +29,11 @@ internal class DebugNavigationComponentImpl @Inject constructor(
         }
     }
 
-    override fun getDebugMenuItems(): List<DebugMenuItem> = emptyList()
-//    override fun getDebugMenuItems(): List<DebugMenuItem> = listOf(
-//        DebugMenuItem(R.string.debug_menu_debug, R.drawable.debug_list_debug, MENU_DEBUG),
-//        DebugMenuItem(R.string.debug_menu_styleguide, R.drawable.debug_list_styleguide, MENU_STYLEGUIDE),
-//        DebugMenuItem(R.string.debug_menu_ads_config, R.drawable.debug_list_adverts, MENU_ADVERTS)
-//    )
+    override fun getDebugMenuItems(): List<DebugMenuItem> = listOf(
+        DebugMenuItem(R.string.debug_menu_debug, R.drawable.debug_list_debug, MENU_DEBUG),
+        DebugMenuItem(R.string.debug_menu_styleguide, R.drawable.debug_list_styleguide, MENU_STYLEGUIDE),
+        DebugMenuItem(R.string.debug_menu_ads_config, R.drawable.debug_list_adverts, MENU_ADVERTS)
+    )
 
     companion object {
         internal const val MENU_DEBUG = "debug"
