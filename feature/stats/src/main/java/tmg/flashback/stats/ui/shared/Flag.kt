9cac84e1d16e3cee80968294516c498d1f124e33
Jordan Fisher <thementalgoose@gmail.com>
2023-02-19 11:51:34 +0000

    Ignores missing resource exception

--- feature/stats/src/main/java/tmg/flashback/stats/ui/shared/Flag.kt
+++ feature/stats/src/main/java/tmg/flashback/stats/ui/shared/Flag.kt
@@ -1,8 +1,10 @@
 package tmg.flashback.stats.ui.shared
 
+import android.content.res.Resources.NotFoundException
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
