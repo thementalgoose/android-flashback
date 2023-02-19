f08c213d2cf54887247c623bdc3c161be4c2870d
Jordan Fisher <thementalgoose@gmail.com>
2023-02-19 11:51:34 +0000

    Ignores missing resource exception

--- formula1/src/main/java/tmg/flashback/formula1/utils/NationalityUtils.kt
+++ formula1/src/main/java/tmg/flashback/formula1/utils/NationalityUtils.kt
@@ -1,13 +1,16 @@
 package tmg.flashback.formula1.utils
 
 import java.util.Locale
+import java.util.MissingResourceException
 
 fun String.toAlpha2ISO(): String? {
     return Locale.getISOCountries()
         .firstOrNull { isCountryCodesEqual(this, it) }
 }
 
-private fun isCountryCodesEqual(alpha3: String, alpha2: String): Boolean {
-    return alpha3.equals(Locale("", alpha2).isO3Country, ignoreCase = true)
+private fun isCountryCodesEqual(alpha3: String, alpha2: String) = try {
+    alpha3.equals(Locale("", alpha2).isO3Country, ignoreCase = true)
+} catch (e: MissingResourceException) {
+    false
 }
 
