b907f0d71e6fbb057934b0a0136f79ba40b8beed
Jordan Fisher <thementalgoose@gmail.com>
2023-03-26 18:49:20 +0100

    Moves RSS navigation to contract

--- feature/rss/core/src/main/java/tmg/flashback/rss/RssNavigationComponent.kt
+++ feature/rss/core/src/main/java/tmg/flashback/rss/RssNavigationComponent.kt
@@ -2,15 +2,3 @@ package tmg.flashback.rss
 
 import tmg.flashback.navigation.NavigationDestination
 import tmg.flashback.navigation.Screen
-
-val tmg.flashback.navigation.Screen.Settings.RSS: tmg.flashback.navigation.NavigationDestination
-    get() = tmg.flashback.navigation.NavigationDestination("settings/rss", popUpTo = "settings")
-
-val tmg.flashback.navigation.Screen.Settings.RSSConfigure: tmg.flashback.navigation.NavigationDestination
-    get() = tmg.flashback.navigation.NavigationDestination(
-        "settings/rss/configure",
-        popUpTo = "settings/rss"
-    )
-
-val tmg.flashback.navigation.Screen.RSS: tmg.flashback.navigation.NavigationDestination
-    get() = tmg.flashback.navigation.NavigationDestination("rss", launchSingleTop = true)
