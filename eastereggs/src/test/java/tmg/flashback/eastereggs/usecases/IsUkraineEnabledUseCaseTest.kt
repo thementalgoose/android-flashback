2346ddaffb5b2f4347b363e6b3084e3d5db7b580
Jordan Fisher <thementalgoose@gmail.com>
2023-05-05 18:15:20 +0100

    Adds IsUkraineEnabledUseCase

---
+++ eastereggs/src/test/java/tmg/flashback/eastereggs/usecases/IsUkraineEnabledUseCaseTest.kt
@@ -0,0 +1,33 @@
+package tmg.flashback.eastereggs.usecases
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+import tmg.flashback.eastereggs.repository.EasterEggsRepository
+
+internal class IsUkraineEnabledUseCaseTest {
+
+    private val mockEasterEggsRepository: EasterEggsRepository = mockk(relaxed = true)
+
+    private lateinit var underTest: IsUkraineEnabledUseCase
+
+    private fun initUnderTest() {
+        underTest = IsUkraineEnabledUseCase(
+            easterEggsRepository = mockEasterEggsRepository
+        )
+    }
+
+    @Test
+    fun `is enabled returns status from repository`() {
+        every { mockEasterEggsRepository.isUkraineEnabled } returns true
+
+        initUnderTest()
+        assertTrue(underTest())
+
+        verify {
+            mockEasterEggsRepository.isUkraineEnabled
+        }
+    }
+}
