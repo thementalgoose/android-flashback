da73b6d039df4874f2bcd9d04dc85fdcd59605ea
Jordan Fisher <thementalgoose@gmail.com>
2023-05-06 22:37:16 +0100

    Migrates maintenance repository should force upgrade flag to use case

--- feature/maintenance/core/src/main/java/tmg/flashback/maintenance/di/MaintenanceModule.kt
+++ feature/maintenance/core/src/main/java/tmg/flashback/maintenance/di/MaintenanceModule.kt
@@ -6,11 +6,16 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import tmg.flashback.maintenance.MaintenanceNavigationComponentImpl
 import tmg.flashback.maintenance.contract.MaintenanceNavigationComponent
+import tmg.flashback.maintenance.contract.usecases.ShouldForceUpgradeUseCase
+import tmg.flashback.maintenance.usecases.ShouldForceUpgradeUseCaseImpl
 
 @Module
 @InstallIn(SingletonComponent::class)
-class MaintenanceModule {
+internal class MaintenanceModule {
 
     @Provides
     fun provideMaintenanceNavigationComponent(impl: MaintenanceNavigationComponentImpl): MaintenanceNavigationComponent = impl
+
+    @Provides
+    fun provideShouldForceUpgradeUseCase(impl: ShouldForceUpgradeUseCaseImpl): ShouldForceUpgradeUseCase = impl
 }
