4917a7334a2d3551a19f47ec7f5d986e1eb9a577
Jordan Fisher <thementalgoose@gmail.com>
2023-05-15 14:29:01 +0100

    WIP Adding sprint shootout UI

--- presentation/style/src/main/java/tmg/flashback/style/Colors.kt
+++ presentation/style/src/main/java/tmg/flashback/style/Colors.kt
@@ -182,15 +182,7 @@ fun AppColors.dynamic(colorScheme: ColorScheme, isLightMode: Boolean) = copy(
         )
     },
     backgroundPrimary = colorScheme.background,
-    backgroundSecondary = when (isLightMode) {
-        true -> colorScheme.surface
-        false -> colorScheme.background.copy(
-            alpha = colorScheme.background.alpha,
-            red = colorScheme.background.red * 0.8f,
-            green = colorScheme.background.green * 0.8f,
-            blue = colorScheme.background.blue * 0.8f
-        )
-    },
+    backgroundSecondary = colorScheme.surfaceColorAtElevation(3.dp),
     backgroundTertiary = colorScheme.primaryContainer,
     backgroundPrimaryInverse = colorScheme.inverseSurface,
     backgroundSecondaryInverse = colorScheme.inversePrimary,
