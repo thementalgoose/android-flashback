64e80b31b451eb8ea160ed49d3ecd9b95266def9
Jordan Fisher <thementalgoose@gmail.com>
2023-05-15 14:18:51 +0100

    Updates sprint shootout initial UI

---
+++ feature/weekend/core/src/main/java/tmg/flashback/weekend/ui/sprintquali/SprintQualifyingViewModel.kt
@@ -0,0 +1,109 @@
+package tmg.flashback.weekend.ui.sprintquali
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import tmg.flashback.drivers.contract.DriverSeason
+import tmg.flashback.drivers.contract.with
+import tmg.flashback.formula1.constants.Formula1.currentSeasonYear
+import tmg.flashback.formula1.model.Driver
+import tmg.flashback.formula1.model.Race
+import tmg.flashback.formula1.model.QualifyingType
+import tmg.flashback.navigation.Screen
+import tmg.flashback.domain.repo.RaceRepository
+import tmg.flashback.formula1.model.SprintQualifyingType
+import javax.inject.Inject
+
+interface SprintQualifyingViewModelInputs {
+    fun load(season: Int, round: Int)
+    fun clickDriver(result: Driver)
+}
+
+interface SprintQualifyingViewModelOutputs {
+    val list: LiveData<List<SprintQualifyingModel>>
+    val headersToShow: LiveData<SprintQualifyingHeader>
+}
+
+typealias SprintQualifyingHeader = Triple<Boolean, Boolean, Boolean>
+
+@HiltViewModel
+class SprintQualifyingViewModel @Inject constructor(
+    private val raceRepository: RaceRepository,
+    private val navigator: tmg.flashback.navigation.Navigator,
+    private val ioDispatcher: CoroutineDispatcher
+): ViewModel(), SprintQualifyingViewModelInputs, SprintQualifyingViewModelOutputs {
+
+    val inputs: SprintQualifyingViewModelInputs = this
+    val outputs: SprintQualifyingViewModelOutputs = this
+
+    private val seasonRound: MutableStateFlow<Pair<Int, Int>?> = MutableStateFlow(null)
+    override val list: LiveData<List<SprintQualifyingModel>> = seasonRound
+        .filterNotNull()
+        .flatMapLatest { (season, round) -> raceRepository.getRace(season, round) }
+        .flowOn(ioDispatcher)
+        .map { race ->
+            if (race == null || race.qualifying.isEmpty()) {
+                val list = mutableListOf<SprintQualifyingModel>().apply {
+                    if ((seasonRound.value?.first ?: currentSeasonYear) >= currentSeasonYear) {
+                        add(SprintQualifyingModel.NotAvailableYet)
+                    } else {
+                        add(SprintQualifyingModel.NotAvailable)
+                    }
+                }
+                return@map list
+            }
+            headersToShow.postValue(
+                SprintQualifyingHeader(
+                    first = true,
+                    second = true,
+                    third = true
+                )
+            )
+
+            return@map race.getSprintShootout()
+        }
+        .asLiveData(viewModelScope.coroutineContext)
+
+    override val headersToShow: MutableLiveData<SprintQualifyingHeader> = MutableLiveData()
+
+    override fun load(season: Int, round: Int) {
+        seasonRound.value = Pair(season, round)
+    }
+
+    private fun Race.getSprintShootout(): List<SprintQualifyingModel> {
+        val list = this.sprint.qualifying.firstOrNull { it.label == SprintQualifyingType.SQ3 } ?: return emptyList()
+
+        return list.results.map {
+            val overview = driverOverview(it.driver.driver.id)
+            return@map SprintQualifyingModel.Result(
+                driver = it.driver,
+                finalQualifyingPosition = overview?.qualified,
+                sq1 = overview?.sprintQ1,
+                sq2 = overview?.sprintQ2,
+                sq3 = overview?.sprintQ3,
+                grid = overview?.race?.grid
+            )
+        }
+    }
+
+    override fun clickDriver(result: Driver) {
+        val season = seasonRound.value?.first ?: return
+        navigator.navigate(
+            Screen.DriverSeason.with(
+                driverId = result.id,
+                driverName = result.name,
+                season = season
+            ))
+    }
+
+}
---
+++ feature/weekend/core/src/test/java/tmg/flashback/weekend/ui/sprintquali/SprintQualifyingViewModelTest.kt
@@ -0,0 +1,100 @@
+package tmg.flashback.weekend.ui.sprintquali
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.flow
+import org.junit.jupiter.api.Test
+import tmg.flashback.domain.repo.RaceRepository
+import tmg.flashback.drivers.contract.DriverSeason
+import tmg.flashback.drivers.contract.with
+import tmg.flashback.formula1.model.QualifyingResult
+import tmg.flashback.formula1.model.Race
+import tmg.flashback.formula1.model.model
+import tmg.flashback.navigation.Navigator
+import tmg.flashback.navigation.Screen
+import tmg.testutils.BaseTest
+import tmg.testutils.livedata.test
+import tmg.testutils.livedata.testObserve
+import java.time.Year
+
+internal class SprintQualifyingViewModelTest: BaseTest() {
+
+    private val mockRaceRepository: RaceRepository = mockk(relaxed = true)
+    private val mockNavigator: Navigator = mockk(relaxed = true)
+
+    private lateinit var underTest: SprintQualifyingViewModel
+
+    private fun initUnderTest() {
+        underTest = SprintQualifyingViewModel(
+            raceRepository = mockRaceRepository,
+            navigator = mockNavigator,
+            ioDispatcher = coroutineScope.testDispatcher
+        )
+    }
+
+    @Test
+    fun `loading view with no race results in same year shows not available yet`() {
+        val currentSeason = Year.now().value
+        every { mockRaceRepository.getRace(currentSeason, 1) } returns flow { emit(null) }
+
+        initUnderTest()
+        underTest.load(currentSeason, 1)
+
+        underTest.outputs.list.test {
+            assertValue(listOf(
+                SprintQualifyingModel.NotAvailableYet
+            ))
+        }
+    }
+
+    @Test
+    fun `loading view with no race results in different year shows not available`() {
+        val currentSeason = 2020
+        every { mockRaceRepository.getRace(currentSeason, 1) } returns flow { emit(null) }
+
+        initUnderTest()
+        underTest.load(currentSeason, 1)
+
+        underTest.outputs.list.test {
+            assertValue(listOf(
+                SprintQualifyingModel.NotAvailable
+            ))
+        }
+    }
+
+    @Test
+    fun `loading view with list of results for q1q2q3`() {
+        val currentSeason = 2020
+        every { mockRaceRepository.getRace(currentSeason, 1) } returns flow { emit(Race.model()) }
+
+        initUnderTest()
+        underTest.load(currentSeason, 1)
+
+        underTest.outputs.list.test {
+            assertValue(listOf(
+                SprintQualifyingModel.Result.model()
+            ))
+        }
+    }
+
+    @Test
+    fun `clicking qualifying result launches stats navigation component`() {
+        initUnderTest()
+        underTest.load(2020, 1)
+
+        val input = QualifyingResult.model()
+        underTest.inputs.clickDriver(input.driver.driver)
+
+        underTest.outputs.list.testObserve()
+
+        verify {
+            mockNavigator.navigate(
+                Screen.DriverSeason.with(
+                    driverId = input.driver.driver.id,
+                    driverName = input.driver.driver.name,
+                    season = 2020
+                ))
+        }
+    }
+}
