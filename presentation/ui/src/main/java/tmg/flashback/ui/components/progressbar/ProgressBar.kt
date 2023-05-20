89a34fcb065ae7e2b1aa4c8376b40e15d2641f84
Jordan Fisher <thementalgoose@gmail.com>
2023-05-20 18:21:07 +0100

    Adds accessibility to ProgressBar

--- presentation/ui/src/main/java/tmg/flashback/ui/components/progressbar/ProgressBar.kt
+++ presentation/ui/src/main/java/tmg/flashback/ui/components/progressbar/ProgressBar.kt
@@ -21,14 +21,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import tmg.flashback.style.AppTheme
 import tmg.flashback.style.AppThemePreview
+import tmg.flashback.ui.R
 import tmg.flashback.ui.utils.MeasureTextWidth
+import tmg.flashback.ui.utils.pointsDisplay
 import tmg.utilities.utils.ColorUtils
 import tmg.utilities.utils.ColorUtils.Companion.contrastTextLight
+import kotlin.math.roundToInt
 
 @Composable
 fun ProgressBar(
@@ -48,10 +55,14 @@ fun ProgressBar(
 ) {
     val endProgress = if (endProgress.isNaN()) { 0f } else { endProgress }
 
+    val contentDescription = pluralStringResource(id = R.plurals.race_points, count = endProgress.roundToInt(), endProgress.toDouble().pointsDisplay())
     BoxWithConstraints(
         modifier = modifier
             .defaultMinSize(minHeight = 48.dp)
             .fillMaxWidth()
+            .clearAndSetSemantics {
+                this.contentDescription = contentDescription
+            }
             .clip(RoundedCornerShape(AppTheme.dimens.radiusSmall))
     ) {
         val progressState = remember { mutableStateOf(initialValue) }
