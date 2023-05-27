ace45cb1252cfe068a1edde63d6c625708793d45
Jordan Fisher <thementalgoose@gmail.com>
2023-05-27 18:08:25 +0100

    WIP Accessibility cleanup

---
+++ formula1/src/main/java/tmg/flashback/formula1/utils/AccessibilityUtils.kt
@@ -0,0 +1,49 @@
+package tmg.flashback.formula1.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import tmg.flashback.formula1.R
+import tmg.flashback.formula1.model.DriverEntry
+import tmg.flashback.formula1.model.QualifyingResult
+import tmg.flashback.formula1.model.RaceResult
+import tmg.utilities.extensions.ordinalAbbreviation
+import kotlin.math.abs
+
+object AccessibilityUtils {
+
+    @Composable
+    fun delta(started: Int, finished: Int) = when {
+        started < finished -> stringResource(R.string.ab_positions_lost, started, finished, abs(finished - started))
+        started > finished -> stringResource(R.string.ab_positions_gained, started, finished, abs(started - finished))
+        else -> stringResource(R.string.ab_positions_neutral)
+    }
+
+    @Composable
+    fun RaceResult.overview(): String {
+        val fastestLap = if (this.fastestLap?.rank == 1) {
+            stringResource(R.string.ab_result_fastest_lap)
+        } else {
+            ""
+        }
+
+        return stringResource(R.string.ab_result_race_overview,
+            this.finish.ordinalAbbreviation,
+            this.driver.driver.name,
+            this.driver.constructor.name
+        )
+    }
+
+    @Composable
+    fun DriverEntry.qualified(qualified: Int?) = if (qualified == null) {
+        stringResource(R.string.ab_result_qualifying_overview_dnq,
+            this.driver.name,
+            this.constructor.name
+        )
+    } else {
+        stringResource(R.string.ab_result_qualifying_overview,
+            this.driver.name,
+            this.driver.name,
+            qualified.ordinalAbbreviation
+        )
+    }
+}
