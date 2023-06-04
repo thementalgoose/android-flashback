a751ee0247421d5dfd6174b1eae079c2430f19bc
Jordan Fisher <thementalgoose@gmail.com>
2023-06-04 17:11:43 +0100

    Adds some logging to widget updating

--- feature/widgets/src/main/java/tmg/flashback/widgets/upnext/UpNextWidgetActions.kt
+++ feature/widgets/src/main/java/tmg/flashback/widgets/upnext/UpNextWidgetActions.kt
@@ -1,14 +1,13 @@
 package tmg.flashback.widgets.upnext
 
 import android.appwidget.AppWidgetManager
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.glance.GlanceId
 import androidx.glance.action.ActionParameters
 import androidx.glance.appwidget.action.ActionCallback
 import tmg.flashback.widgets.di.WidgetsEntryPoints
-import tmg.flashback.widgets.updateWidgets
 import tmg.flashback.widgets.utils.appWidgetId
 
 internal class UpNextWidgetOpenAll: ActionCallback {
@@ -17,6 +16,7 @@ internal class UpNextWidgetOpenAll: ActionCallback {
         glanceId: GlanceId,
         parameters: ActionParameters
     ) {
+        Log.i("UpNextWidget", "Opening app for $glanceId (${glanceId.appWidgetId})")
         val intent = WidgetsEntryPoints.get(context).widgetsNavigationComponent().launchApp(context)
         context.startActivity(intent)
     }
@@ -28,7 +28,7 @@ internal class UpNextWidgetRefreshWidget: ActionCallback {
         glanceId: GlanceId,
         parameters: ActionParameters
     ) {
-
+        Log.i("UpNextWidget", "Refresh widget action for $glanceId (${glanceId.appWidgetId})")
         val intent = Intent(context, UpNextWidgetReceiver::class.java)
         intent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, glanceId.appWidgetId)
