bddc37553f78c14f0afabbfe2159104fa5411d28
Jordan Fisher <thementalgoose@gmail.com>
2023-06-15 15:20:37 +0100

    Adds entry point accessor for crash reporting flag issues as non-fatal

--- presentation/ui/src/main/java/tmg/flashback/ui/components/flag/Flag.kt
+++ presentation/ui/src/main/java/tmg/flashback/ui/components/flag/Flag.kt
@@ -1,11 +1,20 @@
 package tmg.flashback.ui.components.flag
 
+import android.content.res.Resources.NotFoundException
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.hilt.navigation.compose.hiltViewModel
+import tmg.flashback.crash_reporting.di.CrashModule
+import tmg.flashback.style.AppTheme
 import tmg.flashback.style.AppThemePreview
 import tmg.flashback.style.annotations.PreviewTheme
 import tmg.flashback.ui.R
@@ -18,17 +27,32 @@ fun Flag(
     modifier: Modifier = Modifier,
     nationality: String? = null,
 ) {
+    val context = LocalContext.current
     val resourceId = when (isInPreview()) {
         true -> R.drawable.gb
-        false -> LocalContext.current.getFlagResourceAlpha3(iso)
+        false -> context.getFlagResourceAlpha3(iso)
     }
 
-    Image(
-        modifier = modifier,
-        painter = painterResource(id = resourceId),
-        contentDescription = nationality,
-        contentScale = ContentScale.Fit,
-    )
+    if (resourceId != 0) {
+        Image(
+            modifier = modifier,
+            painter = painterResource(id = resourceId),
+            contentDescription = nationality,
+            contentScale = ContentScale.Fit,
+        )
+    } else {
+        Box(
+            modifier = modifier
+                .background(AppTheme.colors.primary.copy(alpha = 0.3f))
+                .clearAndSetSemantics {
+                    this.contentDescription = nationality ?: iso
+                }
+        )
+        DisposableEffect(key1 = Unit, effect = {
+            CrashModule.entryPoints(context).crashManager().logException(NotFoundException("Flag resource for '$iso' ($nationality) not found!"))
+            return@DisposableEffect onDispose {  }
+        })
+    }
 }
 
 @PreviewTheme
