a92fd7c9050c673d5c4396ead70699113f255db5
Jordan Fisher <thementalgoose@gmail.com>
2023-06-23 13:13:55 +0100

    Updates compileSdkVersion to compileSdk

--- eastereggs/src/test/java/tmg/flashback/eastereggs/usecases/IsUkraineEnabledUseCaseTest.kt
+++ eastereggs/src/test/java/tmg/flashback/eastereggs/usecases/IsUkraineEnabledUseCaseTest.kt
@@ -3,8 +3,8 @@ package tmg.flashback.eastereggs.usecases
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 import tmg.flashback.eastereggs.repository.EasterEggsRepository
 
 internal class IsUkraineEnabledUseCaseTest {
