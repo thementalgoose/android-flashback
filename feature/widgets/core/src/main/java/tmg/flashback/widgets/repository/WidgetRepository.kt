d3e86a8f7aed80748bffbf3299fefe360c825465
Jordan Fisher <thementalgoose@gmail.com>
2023-07-07 16:31:28 +0100

    Adds setting config field for the widget

--- feature/widgets/core/src/main/java/tmg/flashback/widgets/repository/WidgetRepository.kt
+++ feature/widgets/core/src/main/java/tmg/flashback/widgets/repository/WidgetRepository.kt
@@ -12,6 +12,7 @@ class WidgetRepository @Inject constructor(
     companion object {
         private fun widgetUpNextShowBackground(appWidgetId: Int) = "widget_upnext_show_background_$appWidgetId"
         private fun widgetUpNextShowWeather(appWidgetId: Int) = "widget_upnext_show_weather_$appWidgetId"
+        private fun widgetUpNextClickToEvent(appWidgetId: Int) = "widget_upnext_click_to_event_$appWidgetId"
     }
 
     fun setShowBackground(appWidgetId: Int, showBackground: Boolean) {
@@ -28,5 +29,13 @@ class WidgetRepository @Inject constructor(
 
     fun getShowWeather(appWidgetId: Int): Boolean {
         return preferenceManager.getBoolean(widgetUpNextShowWeather(appWidgetId))
+    }
+
+    fun setClickToEvent(appWidgetId: Int, clickToEvent: Boolean) {
+        preferenceManager.save(widgetUpNextClickToEvent(appWidgetId), clickToEvent)
+    }
+
+    fun getClickToEvent(appWidgetId: Int): Boolean {
+        return preferenceManager.getBoolean(widgetUpNextClickToEvent(appWidgetId))
     }
 }
