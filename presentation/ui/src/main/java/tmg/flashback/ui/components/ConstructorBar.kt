915a138acc49aaa84e23456ed3d70a3391ff2416
Jordan Fisher <thementalgoose@gmail.com>
2023-07-17 10:50:24 +0100

    Adds preview for constructor bar

--- presentation/ui/src/main/java/tmg/flashback/ui/components/ConstructorBar.kt
+++ presentation/ui/src/main/java/tmg/flashback/ui/components/ConstructorBar.kt
@@ -1,12 +1,19 @@
 package tmg.flashback.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import tmg.flashback.style.AppTheme
+import tmg.flashback.style.AppThemePreview
 
 fun Modifier.constructorIndicator(
     colour: Color,
@@ -20,4 +27,16 @@ fun Modifier.constructorIndicator(
             )
         }
         .padding(start = size)
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    AppThemePreview {
+        Box(modifier = Modifier
+            .size(width = 50.dp, height = 16.dp)
+            .background(Color.White)
+            .constructorIndicator(AppTheme.colors.primary)
+        )
+    }
 }
