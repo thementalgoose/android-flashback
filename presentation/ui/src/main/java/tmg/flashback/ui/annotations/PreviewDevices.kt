29ac3c7ea17e48c6a16f54a9e6edf45ee80ed944
Jordan Fisher <thementalgoose@gmail.com>
2023-07-29 00:07:07 +0100

    Adds preview sizes and window size class fakes

--- presentation/ui/src/main/java/tmg/flashback/ui/annotations/PreviewDevices.kt
+++ presentation/ui/src/main/java/tmg/flashback/ui/annotations/PreviewDevices.kt
@@ -12,9 +12,25 @@ import androidx.compose.ui.tooling.preview.Preview
     device = "spec:shape=Normal,width=1200,height=800,unit=dp,dpi=480",
     name = "Tablet"
 )
+annotation class PreviewDevices
+
+@Preview(
+    showBackground = true,
+    device = "spec:shape=Normal,width=1200,height=800,unit=dp,dpi=480",
+    name = "Tablet"
+)
+annotation class PreviewTablet
+
+@Preview(
+    showBackground = true,
+    device = "spec:shape=Normal,width=480,height=720,unit=dp,dpi=480",
+    name = "Phone"
+)
+annotation class PreviewPhone
+
 @Preview(
     showBackground = true,
     device = "spec:shape=Normal,width=673,height=841,unit=dp,dpi=480",
     name = "Foldable"
 )
-annotation class PreviewDevices
+annotation class PreviewFoldable
