b7501d5ada3db26061103765864b563abdeddd32
Jordan Fisher <thementalgoose@gmail.com>
2023-07-29 14:46:15 +0100

    Adds web browser to tablet ui

--- presentation/ui/src/main/java/tmg/flashback/ui/components/layouts/MasterDetailPane.kt
+++ presentation/ui/src/main/java/tmg/flashback/ui/components/layouts/MasterDetailPane.kt
@@ -1,15 +1,18 @@
 package tmg.flashback.ui.components.layouts
 
+import android.view.RoundedCorner
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import tmg.flashback.style.AppTheme
 import tmg.flashback.style.AppThemePreview
@@ -22,7 +25,7 @@ import tmg.flashback.ui.utils.fakeExpandedWindowSizeClass
 import tmg.flashback.ui.utils.fakeMediumWindowSizeClass
 
 private val backgroundColor: Color
-    @Composable get() = AppTheme.colors.backgroundContainer
+    @Composable get() = AppTheme.colors.backgroundPrimary
 
 @Composable
 fun MasterDetailPane(
@@ -30,6 +33,7 @@ fun MasterDetailPane(
     main: @Composable BoxScope.() -> Unit,
     modifier: Modifier = Modifier,
     content: @Composable (BoxScope.() -> Unit)? = null,
+    showContent: Boolean = content != null,
 ) {
     Box(modifier = modifier
         .background(backgroundColor)
@@ -38,18 +42,27 @@ fun MasterDetailPane(
             Box(modifier = Modifier.weight(4f)) {
                 main()
             }
-            if (windowSizeClass.widthSizeClass == WindowWidthSizeClass.Expanded && content != null) {
-                Box(modifier = Modifier.weight(5f)) {
-                    content()
+            if (windowSizeClass.widthSizeClass == WindowWidthSizeClass.Expanded) {
+                Box(modifier = Modifier
+                    .weight(5f)
+                    .padding(
+                        vertical = AppTheme.dimens.medium,
+                        horizontal = AppTheme.dimens.medium
+                    )
+                    .clip(RoundedCornerShape(AppTheme.dimens.radiusMedium))
+                ) {
+                    if (showContent) {
+                        content?.invoke(this)
+                    }
                 }
             }
         }
-        if (windowSizeClass.widthSizeClass != WindowWidthSizeClass.Expanded && content != null) {
+        if (windowSizeClass.widthSizeClass != WindowWidthSizeClass.Expanded && showContent) {
             Box(modifier = Modifier
                 .fillMaxSize()
                 .background(backgroundColor)
             ) {
-                content()
+                content?.invoke(this)
             }
         }
     }
