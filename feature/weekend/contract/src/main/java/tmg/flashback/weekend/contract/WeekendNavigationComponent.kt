3713e96c1788f6684db0da690cb450344b8b4863
Jordan Fisher <thementalgoose@gmail.com>
2023-09-04 19:33:50 +0100

    Updates default tab logic in weekend view model

--- feature/weekend/contract/src/main/java/tmg/flashback/weekend/contract/WeekendNavigationComponent.kt
+++ feature/weekend/contract/src/main/java/tmg/flashback/weekend/contract/WeekendNavigationComponent.kt
@@ -1,14 +1,31 @@
 package tmg.flashback.weekend.contract
 
 import kotlinx.serialization.json.Json
+import org.threeten.bp.Year
 import tmg.flashback.navigation.NavigationDestination
 import tmg.flashback.navigation.Screen
 import tmg.flashback.weekend.contract.model.ScreenWeekendData
+import tmg.flashback.weekend.contract.model.ScreenWeekendNav
 
 @JvmInline
-value class ScreenWeekend(val route: String)
-val Screen.Weekend get() = ScreenWeekend("weekend/{data}")
+value class ScreenWeekend(val route: String) {
+    companion object {
+        const val DATA = "data"
+        const val TAB = "tab"
+    }
+}
+val Screen.Weekend get() = ScreenWeekend("weekend/{data}?tab={tab}")
 
-fun ScreenWeekend.with(weekendInfo: ScreenWeekendData) = NavigationDestination(
-    this@with.route.replace("{data}", Json.encodeToString(ScreenWeekendData.serializer(), weekendInfo))
+fun ScreenWeekend.with(weekendInfo: ScreenWeekendData, tab: ScreenWeekendNav? = weekendInfo.getTab()) = NavigationDestination(
+    this@with.route
+        .replace("{data}", Json.encodeToString(ScreenWeekendData.serializer(), weekendInfo))
+        .replace("{tab}", tab?.name ?: "")
 )
+
+private fun ScreenWeekendData.getTab(): ScreenWeekendNav {
+    return when {
+        season > Year.now().value -> ScreenWeekendNav.SCHEDULE
+        season < Year.now().value -> ScreenWeekendNav.RACE
+        else -> ScreenWeekendNav.SCHEDULE
+    }
+}
