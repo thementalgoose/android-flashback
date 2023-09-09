5d46c42517c2f5de8a6b01181e797a7d7528f89f
Jordan Fisher <thementalgoose@gmail.com>
2023-09-09 21:50:53 +0100

    Updated compile sdk and target sdk to 34, refactors notification flow to support multiple runtime permissions

---
+++ presentation/ui/src/main/java/tmg/flashback/ui/Permissions.kt
@@ -0,0 +1,54 @@
+package tmg.flashback.ui
+
+import android.Manifest
+import android.content.Intent
+import android.os.Build
+import android.os.Parcelable
+import android.provider.Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
+import androidx.annotation.DrawableRes
+import androidx.annotation.RequiresApi
+import androidx.annotation.StringRes
+import kotlinx.parcelize.Parcelize
+
+sealed class AppPermissions: Parcelable {
+
+    @Parcelize
+    sealed class RuntimePermission(
+        val permission: String,
+        @StringRes
+        val description: Int,
+        @DrawableRes
+        val icon: Int
+    ): AppPermissions() {
+        companion object {
+            fun get(permissionStrings: List<String>): List<RuntimePermission> {
+                return permissionStrings.mapNotNull {
+                    when (it) {
+                        RuntimeNotifications.permission -> RuntimeNotifications
+                        else -> null
+                    }
+                }
+            }
+        }
+    }
+
+    @Parcelize
+    sealed class OpenIntent(
+        val getIntent: () -> Intent,
+    ): AppPermissions()
+
+
+    @Parcelize
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    data object RuntimeNotifications: RuntimePermission(
+        permission = Manifest.permission.POST_NOTIFICATIONS,
+        description = R.string.permissions_rationale_runtime_notifications_description,
+        icon = R.drawable.ic_permission_notifications
+    )
+
+    @Parcelize
+    @RequiresApi(Build.VERSION_CODES.S)
+    data object ScheduleExactAlarms: OpenIntent(
+        getIntent = { Intent(ACTION_REQUEST_SCHEDULE_EXACT_ALARM) }
+    )
+}
--- presentation/ui/src/main/java/tmg/flashback/ui/repository/PermissionRepository.kt
+++ presentation/ui/src/main/java/tmg/flashback/ui/repository/PermissionRepository.kt
@@ -1,10 +1,14 @@
 package tmg.flashback.ui.repository
 
 import android.Manifest
+import android.app.AlarmManager
+import android.content.Context.ALARM_SERVICE
 import android.content.pm.PackageManager
 import android.os.Build
+import androidx.core.app.AlarmManagerCompat
 import androidx.core.content.ContextCompat
 import tmg.flashback.navigation.ActivityProvider
+import tmg.flashback.ui.AppPermissions
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,6 +16,19 @@ import javax.inject.Singleton
 class PermissionRepository @Inject constructor(
     private val topActivityProvider: ActivityProvider
 ) {
+
+    fun isEnabled(permission: AppPermissions.RuntimePermission): Boolean {
+        return when (permission) {
+            AppPermissions.RuntimeNotifications -> isRuntimeNotificationsEnabled
+        }
+    }
+
+    fun isEnabled(permission: AppPermissions.OpenIntent): Boolean {
+        return when (permission) {
+            AppPermissions.ScheduleExactAlarms -> isExactAlarmEnabled
+        }
+    }
+
     val isRuntimeNotificationsEnabled: Boolean
         get() {
             val baseActivity = topActivityProvider.activity ?: return false
@@ -20,6 +37,16 @@ class PermissionRepository @Inject constructor(
                     PackageManager.PERMISSION_GRANTED -> true
                     else -> false
                 }
+            } else {
+                true
+            }
+        }
+
+    val isExactAlarmEnabled: Boolean
+        get() {
+            val alarmManager = topActivityProvider.activity?.getSystemService(ALARM_SERVICE) as AlarmManager
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                alarmManager.canScheduleExactAlarms()
             } else {
                 true
             }
