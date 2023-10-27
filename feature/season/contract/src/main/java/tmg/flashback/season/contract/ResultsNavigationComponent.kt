845da8f0cc091caef0850d2ab52858dd8911d7d0
Jordan Fisher <thementalgoose@gmail.com>
2023-10-27 13:14:28 +0100

    Sets single top for main navigation results, fixes back behaviour location

--- feature/season/contract/src/main/java/tmg/flashback/season/contract/ResultsNavigationComponent.kt
+++ feature/season/contract/src/main/java/tmg/flashback/season/contract/ResultsNavigationComponent.kt
@@ -3,11 +3,11 @@ package tmg.flashback.season.contract
 import tmg.flashback.navigation.NavigationDestination
 import tmg.flashback.navigation.Screen
 
-val Screen.DriverStandings get() = NavigationDestination("results/drivers")
+val Screen.DriverStandings get() = NavigationDestination("results/drivers", launchSingleTop = true, popUpTo = "results/races")
 
-val Screen.ConstructorsStandings get() = NavigationDestination("results/constructors")
+val Screen.ConstructorsStandings get() = NavigationDestination("results/constructors", launchSingleTop = true, popUpTo = "results/races")
 
-val Screen.Races get() = NavigationDestination("results/races")
+val Screen.Races get() = NavigationDestination("results/races", launchSingleTop = true)
 
 interface ResultsNavigationComponent {
 
