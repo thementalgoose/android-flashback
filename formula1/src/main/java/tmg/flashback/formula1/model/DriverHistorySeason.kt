a634ebb192b5ec3a11649594432a2962083c912e
Jordan Fisher <thementalgoose@gmail.com>
2024-06-28 17:39:34 +0100

    Filters out sprint events

--- formula1/src/main/java/tmg/flashback/formula1/model/DriverHistorySeason.kt
+++ formula1/src/main/java/tmg/flashback/formula1/model/DriverHistorySeason.kt
@@ -12,6 +12,7 @@ data class DriverHistorySeason(
 ) {
     val bestFinish: Int? by lazy {
         return@lazy raceOverview
+            .filter { !it.isSprint }
             .filter { it.finished > 0 }
             .minByOrNull { it.finished }
             ?.finished
@@ -19,19 +20,26 @@ data class DriverHistorySeason(
 
     val bestQualifying: Int? by lazy {
         return@lazy raceOverview
+            .filter { !it.isSprint }
             .filter { (it.qualified ?: 0) > 0 }
             .minByOrNull { it.qualified ?: Int.MAX_VALUE }
             ?.qualified
     }
 
     val podiums: Int by lazy {
-        return@lazy raceOverview.count { it.finished in 1..3 }
+        return@lazy raceOverview
+            .filter { !it.isSprint }
+            .count { it.finished in 1..3 }
     }
     val races: Int by lazy {
-        return@lazy raceOverview.size
+        return@lazy raceOverview
+            .filter { !it.isSprint }
+            .size
     }
     val wins: Int by lazy {
-        return@lazy raceOverview.count { it.finished == 1}
+        return@lazy raceOverview
+            .filter { !it.isSprint }
+            .count { it.finished == 1}
     }
 
     val qualifyingPoles: Int by lazy {
@@ -43,6 +51,7 @@ data class DriverHistorySeason(
 
     val finishesInPoints: Int by lazy {
         return@lazy raceOverview
+            .filter { !it.isSprint }
             .count { it.points > 0 }
     }
 
@@ -50,14 +59,21 @@ data class DriverHistorySeason(
         return@lazy races
     }
     val raceFinishes: Int by lazy {
-        return@lazy raceOverview.filter { it.status.isStatusFinished() }.size
+        return@lazy raceOverview
+            .filter { !it.isSprint }
+            .filter { it.status.isStatusFinished() }
+            .size
     }
     val raceRetirements: Int by lazy {
-        return@lazy raceOverview.filter { !it.status.isStatusFinished() }.size
+        return@lazy raceOverview
+            .filter { !it.isSprint }
+            .filter { !it.status.isStatusFinished() }
+            .size
     }
 
     fun totalQualifyingAbove(position: Int): Int {
         return raceOverview
+            .filter { !it.isSprint }
             .count {
                 it.qualified != null && it.qualified >= 1 && it.qualified <= position
             }
@@ -65,6 +81,7 @@ data class DriverHistorySeason(
 
     private fun totalQualifyingIn(position: Int): Int {
         return raceOverview
+            .filter { !it.isSprint }
             .count { it.qualified == position }
     }
 
