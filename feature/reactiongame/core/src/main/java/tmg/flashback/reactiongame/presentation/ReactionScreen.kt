2112d6090fd785548932d942d88b4a130039a1e6
Jordan Fisher <thementalgoose@gmail.com>
2024-10-01 21:44:47 +0100

    Moving reaction game to it's own module, WIP

---
+++ feature/reactiongame/core/src/main/java/tmg/flashback/reactiongame/presentation/ReactionScreen.kt
@@ -0,0 +1,42 @@
+package tmg.flashback.reactiongame.presentation
+
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+fun ReactionScreenVM(
+    actionUpClicked: () -> Unit,
+    windowSizeClass: WindowSizeClass,
+    viewModel: ReactionViewModel = hiltViewModel()
+) {
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+
+    ReactionScreen(
+        actionUpClicked = actionUpClicked,
+        windowSizeClass = windowSizeClass,
+        state = uiState.value
+    )
+}
+
+@Composable
+private fun ReactionScreen(
+    actionUpClicked: () -> Unit,
+    windowSizeClass: WindowSizeClass,
+    state: ReactionUiState,
+) {
+
+}
+
+@Composable
+private fun RaceStartLights() {
+
+}
+
+@Preview
+@Composable
+private fun PreviewLights() {
+
+}
