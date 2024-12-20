40d799384029d61cb6fdbe0746f924b6f25acdb8
Jordan Fisher <thementalgoose@gmail.com>
2024-12-20 19:36:53 +0000

    Updates ad implementation to make it more configurable, aligns all the views to different usages

--- core/ads/ads/src/main/java/tmg/flashback/ads/ads/components/AdvertProvider.kt
+++ core/ads/ads/src/main/java/tmg/flashback/ads/ads/components/AdvertProvider.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import tmg.flashback.style.text.TextBody2
 
@@ -11,20 +12,25 @@ interface AdvertProvider {
 
     @Composable
     fun NativeBannerImpl(
-        horizontalPadding: Boolean,
-        badgeOffset: Boolean,
+        horizontalPadding: Dp,
+        adIconSize: Dp?,
+        adIconSpacing: Dp?,
         adIndex: Int
     )
 
+    // Default params not working with abstract compose functions,
+    //  so null = use default value
     @Composable
     fun NativeBanner(
-        horizontalPadding: Boolean,
-        badgeOffset: Boolean,
+        horizontalPadding: Dp,
+        adIconSize: Dp?,
+        adIconSpacing: Dp?,
         adIndex: Int
     ) {
         NativeBannerImpl(
             horizontalPadding = horizontalPadding,
-            badgeOffset = badgeOffset,
+            adIconSize = adIconSize,
+            adIconSpacing = adIconSpacing,
             adIndex = adIndex
         )
     }
@@ -33,13 +39,13 @@ interface AdvertProvider {
 val fakeAdvertProvider = object : AdvertProvider {
     @Composable
     override fun NativeBannerImpl(
-        horizontalPadding: Boolean,
-        badgeOffset: Boolean,
+        horizontalPadding: Dp,
+        adIconSize: Dp?,
+        adIconSpacing: Dp?,
         adIndex: Int
     ) {
-        Box(Modifier.padding(16.dp)) {
+        Box(Modifier.padding(horizontalPadding)) {
             TextBody2(text = "Advert")
         }
     }
-
 }
