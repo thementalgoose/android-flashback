8f2b183cbcb44444c1cb900f1342d591d8346568
Jordan Fisher <thementalgoose@gmail.com>
2024-12-28 12:54:54 +0000

    Completing review flow

--- core/reviews/src/main/java/tmg/flashback/reviews/manager/AppReviewManager.kt
+++ core/reviews/src/main/java/tmg/flashback/reviews/manager/AppReviewManager.kt
@@ -1,6 +1,10 @@
 package tmg.flashback.reviews.manager
 
+import android.app.Activity
 import android.content.Context
+import com.google.android.play.core.ktx.launchReview
+import com.google.android.play.core.ktx.requestReview
+import com.google.android.play.core.review.ReviewInfo
 import com.google.android.play.core.review.ReviewManagerFactory
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -9,5 +13,13 @@ class AppReviewManager @Inject constructor(
     @ApplicationContext
     private val applicationContext: Context,
 ) {
-    internal val manager = ReviewManagerFactory.create(applicationContext)
+    private val manager = ReviewManagerFactory.create(applicationContext)
+
+    suspend fun requestReview(): ReviewInfo {
+        return manager.requestReview()
+    }
+
+    suspend fun launchReview(activity: Activity, reviewInfo: ReviewInfo) {
+        manager.launchReview(activity, reviewInfo)
+    }
 }
--- core/reviews/src/main/java/tmg/flashback/reviews/usecases/ReviewAppSectionSeenUseCase.kt
+++ core/reviews/src/main/java/tmg/flashback/reviews/usecases/ReviewAppSectionSeenUseCase.kt
@@ -1,4 +1,20 @@
 package tmg.flashback.reviews.usecases
 
-class ReviewAppSectionSeenUseCase {
+import tmg.flashback.reviews.repository.AppReviewRepository
+import javax.inject.Inject
+
+class ReviewAppSectionSeenUseCase @Inject constructor(
+    private val appReviewRepository: AppReviewRepository
+) {
+    operator fun invoke(section: AppSection) {
+        appReviewRepository.sectionsSeen += section
+    }
+}
+
+enum class AppSection(
+    internal val key: String
+) {
+    HOME("home"),
+    DETAILS_QUALIFYING("details_qualifying"),
+    DETAILS_RACE("details_race");
 }
