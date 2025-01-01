b627d180f9486ddd09644615b30bd6fda87d5eac
Jordan Fisher <thementalgoose@gmail.com>
2025-01-01 01:18:00 +0000

    Adds app review prompt to app start

--- core/reviews/src/main/java/tmg/flashback/reviews/usecases/StartReviewUseCase.kt
+++ core/reviews/src/main/java/tmg/flashback/reviews/usecases/StartReviewUseCase.kt
@@ -1,8 +1,10 @@
 package tmg.flashback.reviews.usecases
 
+import android.util.Log
 import tmg.flashback.crashlytics.manager.CrashlyticsManager
 import tmg.flashback.device.ActivityProvider
 import tmg.flashback.googleanalytics.manager.FirebaseAnalyticsManager
+import tmg.flashback.reviews.BuildConfig
 import tmg.flashback.reviews.manager.AppReviewManager
 import tmg.flashback.reviews.repository.AppReviewRepository
 import javax.inject.Inject
@@ -17,18 +19,29 @@ class StartReviewUseCase @Inject constructor(
 
     suspend fun start() {
         try {
-            if (!appReviewRepository.hasPromptedForReview && appReviewRepository.sectionsSeen == AppSection.entries.toSet()) {
-                crashlyticsManager.log("Starting app review prompt")
-                val reviewInfo = appReviewManager.requestReview()
-                val activity = topActivityProvider.activity
-                if (activity != null) {
-                    analyticsManager.logEvent("review_prompt_request")
-                    appReviewRepository.hasPromptedForReview = true
-                    appReviewManager.launchReview(activity, reviewInfo)
-                    analyticsManager.logEvent("review_prompt_complete")
-                } else {
-                    crashlyticsManager.log("Top activity not available")
-                }
+            if (appReviewRepository.hasPromptedForReview) {
+                crashlyticsManager.log("App review prompt - Previously prompted for review, not requesting")
+                return
+            }
+            if (appReviewRepository.sectionsSeen != AppSection.entries.toSet()) {
+                crashlyticsManager.log("App review prompt - Only ${appReviewRepository.sectionsSeen.size} seen, not requesting")
+                return
+            }
+
+            if (BuildConfig.DEBUG) {
+                Log.d("Review", "App Review Prompt, sectionsSeen=${appReviewRepository.sectionsSeen}, hasPrompted=${appReviewRepository.hasPromptedForReview}")
+            }
+
+            crashlyticsManager.log("App review prompt - Requesting review")
+            val reviewInfo = appReviewManager.requestReview()
+            val activity = topActivityProvider.activity
+            if (activity != null) {
+                analyticsManager.logEvent("review_prompt_request")
+                appReviewRepository.hasPromptedForReview = true
+                appReviewManager.launchReview(activity, reviewInfo)
+                analyticsManager.logEvent("review_prompt_complete")
+            } else {
+                crashlyticsManager.log("App review prompt - Not displaying due to top activity not available")
             }
         } catch (e: Exception) {
             crashlyticsManager.logException(ReviewNotPromptedException(e))
