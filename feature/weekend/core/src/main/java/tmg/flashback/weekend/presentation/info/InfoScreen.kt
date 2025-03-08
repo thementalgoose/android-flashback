f7818ff27dc69f04a3ddbeb24e9ece32ecb33771
Jordan Fisher <thementalgoose@gmail.com>
2025-03-08 21:39:20 +0000

    Updates material3 reference

--- feature/weekend/core/src/main/java/tmg/flashback/weekend/presentation/info/InfoScreen.kt
+++ feature/weekend/core/src/main/java/tmg/flashback/weekend/presentation/info/InfoScreen.kt
@@ -1,8 +1,8 @@
 package tmg.flashback.weekend.presentation.info
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
--- presentation/ui/src/main/java/tmg/flashback/ui/components/navigation/NavigationColumn.kt
+++ presentation/ui/src/main/java/tmg/flashback/ui/components/navigation/NavigationColumn.kt
@@ -20,7 +20,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
-import androidx.compose.material.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
