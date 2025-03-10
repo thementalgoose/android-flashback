5dadacb36e931ba13c15f0d89db7d86569c0155a
Jordan Fisher <thementalgoose@gmail.com>
2025-03-10 17:34:08 +0000

    Removes fake preview data

--- feature/widgets/core/src/main/java/tmg/flashback/widgets/utils/PreviewData.kt
+++ feature/widgets/core/src/main/java/tmg/flashback/widgets/utils/PreviewData.kt
@@ -1,16 +1,10 @@
 package tmg.flashback.widgets.utils
 
-import androidx.compose.ui.graphics.Color
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalTime
 import tmg.flashback.formula1.model.OverviewRace
 import tmg.flashback.formula1.model.Schedule
-import tmg.flashback.widgets.presentation.components.WidgetColourData
 
-private val fakeWidgetColourData = WidgetColourData(
-    contentColour = Color.DarkGray,
-    backgroundColor = Color.White
-)
 private val fakeOverviewRace = OverviewRace(
     date = LocalDate.of(2020, 1, 3),
     time = LocalTime.of(15, 0, 0),
