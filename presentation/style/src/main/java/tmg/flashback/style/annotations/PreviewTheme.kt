ffdb9597dcdec1de7594ee6d7667a21b7ced7208
Jordan Fisher <thementalgoose@gmail.com>
2025-04-05 13:01:51 +0100

    Fixes preview annotation theming

--- presentation/style/src/main/java/tmg/flashback/style/annotations/PreviewTheme.kt
+++ presentation/style/src/main/java/tmg/flashback/style/annotations/PreviewTheme.kt
@@ -2,22 +2,20 @@ package tmg.flashback.style.annotations
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(
     showBackground = true,
     name = "Light",
-    backgroundColor = 4294506744,
-    uiMode = UI_MODE_NIGHT_NO,
-    device = Devices.PIXEL_4
+    backgroundColor = 4294506744
 )
 @Preview(
     showBackground = true,
     name = "Dark",
     backgroundColor = 4279769112,
-    uiMode = UI_MODE_NIGHT_YES,
-    device = Devices.PIXEL_4
+    uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL
 )
 annotation class PreviewTheme
 
@@ -25,14 +23,13 @@ annotation class PreviewTheme
     showBackground = true,
     name = "Light",
     backgroundColor = 4294506744,
-    uiMode = UI_MODE_NIGHT_NO,
     device = Devices.NEXUS_10
 )
 @Preview(
     showBackground = true,
     name = "Dark",
     backgroundColor = 4279769112,
-    uiMode = UI_MODE_NIGHT_YES,
+    uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL,
     device = Devices.NEXUS_10
 )
 annotation class PreviewThemeExpanded
