d6f119264dc9b55743ee14622cde9f9a5f9afa77
Jordan Fisher <thementalgoose@gmail.com>
2025-04-05 10:43:49 +0100

    Removes sdk int checks from app shortcuts

--- core/appshortcuts/src/main/java/tmg/flashback/appshortcuts/manager/AppShortcutManager.kt
+++ core/appshortcuts/src/main/java/tmg/flashback/appshortcuts/manager/AppShortcutManager.kt
@@ -18,50 +18,38 @@ class AppShortcutManager @Inject constructor(
 ) {
 
     fun setDynamicShortcuts(shortcuts: List<ShortcutInfo>) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-            ShortcutManagerCompat.setDynamicShortcuts(applicationContext, shortcuts.mapNotNull { buildShortcutModel(it) })
-        }
+        ShortcutManagerCompat.setDynamicShortcuts(applicationContext, shortcuts.mapNotNull { buildShortcutModel(it) })
     }
 
     fun addDynamicShortcut(shortcut: ShortcutInfo) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-            val shortcutModel = buildShortcutModel(shortcut) ?: return
-            ShortcutManagerCompat.pushDynamicShortcut(applicationContext, shortcutModel)
-        }
+        val shortcutModel = buildShortcutModel(shortcut) ?: return
+        ShortcutManagerCompat.pushDynamicShortcut(applicationContext, shortcutModel)
     }
 
     fun removeDynamicShortcut(shortcutId: String) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-            ShortcutManagerCompat.removeDynamicShortcuts(applicationContext, listOf(shortcutId))
-        }
+        ShortcutManagerCompat.removeDynamicShortcuts(applicationContext, listOf(shortcutId))
     }
 
     fun clearDynamicShortcuts() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-            ShortcutManagerCompat.removeAllDynamicShortcuts(applicationContext)
-        }
+        ShortcutManagerCompat.removeAllDynamicShortcuts(applicationContext)
     }
 
     private fun buildShortcutModel(model: ShortcutInfo): ShortcutInfoCompat? {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-            try {
-                ShortcutInfoCompat.Builder(applicationContext, model.id)
-                    .setShortLabel(applicationContext.getString(model.shortLabel))
-                    .setLongLabel(applicationContext.getString(model.longLabel))
-                    .setIcon(IconCompat.createWithResource(applicationContext, model.icon))
-                    .setDisabledMessage(applicationContext.getString(model.unavailableMessage))
-                    .setIntent(model.intentResolver(applicationContext.applicationContext, homeClassProvider.getHomeActivity()).apply {
-                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    })
-                    .build()
-            } catch (e: IncompatibleClassChangeError) {
-                // If it fails to generate the icon for the app shortcut
-                null
-            } catch (e: RuntimeException) {
-                // If some other error happens, not critical functionality
-                null
-            }
-        } else {
+        return try {
+            ShortcutInfoCompat.Builder(applicationContext, model.id)
+                .setShortLabel(applicationContext.getString(model.shortLabel))
+                .setLongLabel(applicationContext.getString(model.longLabel))
+                .setIcon(IconCompat.createWithResource(applicationContext, model.icon))
+                .setDisabledMessage(applicationContext.getString(model.unavailableMessage))
+                .setIntent(model.intentResolver(applicationContext.applicationContext, homeClassProvider.getHomeActivity()).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                })
+                .build()
+        } catch (e: IncompatibleClassChangeError) {
+            // If it fails to generate the icon for the app shortcut
+            null
+        } catch (e: RuntimeException) {
+            // If some other error happens, not critical functionality
             null
         }
     }
