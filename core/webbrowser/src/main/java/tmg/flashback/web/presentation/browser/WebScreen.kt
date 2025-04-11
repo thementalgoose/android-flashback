75558495949b97d562a3c5537b0b1c9316916f4c
Jordan Fisher <thementalgoose@gmail.com>
2025-04-11 21:57:46 +0100

    Adds status bar padding to in app webview

--- core/webbrowser/src/main/java/tmg/flashback/web/presentation/browser/WebScreen.kt
+++ core/webbrowser/src/main/java/tmg/flashback/web/presentation/browser/WebScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -57,6 +58,7 @@ fun WebScreen(
     Column(
         Modifier
             .background(AppTheme.colors.backgroundPrimary)
+            .statusBarsPadding()
             .fillMaxSize()
     ) {
         val titleValue = remember { mutableStateOf(title) }
